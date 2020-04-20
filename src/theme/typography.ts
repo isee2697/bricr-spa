7f627ebc048ca6d68594e0344fbc4b4c21e2dd18
Damian Szczepaniak <damian.szczepaniak@tsh.io>
2020-04-20 18:06:35 +0200

    [BRICR-62] add material iu and basic styles

---
+++ src/theme/typography.ts
@@ -0,0 +1,38 @@
+export const regular = 400;
+export const medium = 500;
+export const semibold = 60;
+export const bold = 700;
+
+export const typography = {
+  fontFamily: 'Poppins, sans-serif',
+  h1: {
+    fontSize: 24,
+    fontWeight: regular,
+    lineHeight: 32,
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: regular,
+    lineHeight: 32,
+  },
+  h3: {
+    fontSize: 16,
+    fontWeight: regular,
+    lineHeight: 24,
+  },
+  h4: {
+    fontSize: 14,
+    fontWeight: regular,
+    lineHeight: 24,
+  },
+  h5: {
+    fontSize: 12,
+    fontWeight: regular,
+    lineHeight: 16,
+  },
+  h6: {
+    fontSize: 10,
+    fontWeight: regular,
+    lineHeight: 16,
+  },
+};
