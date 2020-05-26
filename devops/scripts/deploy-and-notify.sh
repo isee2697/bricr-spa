d040cbe2496d1e3b94f19fb6cfa76fcfb27725ee
Kamil Wojtaszczyk <kamil.wojtaszczyk@tsh.io>
2020-05-26 11:04:00 +0000

    Merged in BRICR-213-pipelines-improvements (pull request #127)
    BRICR-213 adjustments to new Dockerfile, deployment dependencies moved to image
    
    * BRICR-213 adjustments to new Dockerfile, deployment dependencies moved to image
    
    * BRICR-214 added notifications
    
    Approved-by: Andreas Otten <andreas@cubiceyes.com>
    Approved-by: Damian Szczepaniak <damian.szczepaniak@tsh.io>
    Approved-by: Marcin Piela <marcin.piela@thesoftwarehouse.pl>

---
+++ devops/scripts/deploy-and-notify.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+function fail() {
+    curl -X POST --data-urlencode "payload={\"channel\": \"#bricr-notifications\", \"username\": \"Pipelines\", \"text\": \"Hey! Your job <https://bitbucket.org/thesoftwarehouse/bricr-spa/addon/pipelines/home#!/results/${BITBUCKET_BUILD_NUMBER}|failed> :pepecry: check it please.\"}" "${SLACK_WEBHOOK}"
+}
+
+function success()  {
+    curl -X POST --data-urlencode "payload={\"channel\": \"#bricr-notifications\", \"username\": \"Pipelines\", \"text\": \"Hey! Your job <https://bitbucket.org/thesoftwarehouse/bricr-spa/addon/pipelines/home#!/results/${BITBUCKET_BUILD_NUMBER}|finished successfully.> It should be live here ${URL} in a few minutes.\"}" "${SLACK_WEBHOOK}"
+}
+
+export TF_VAR_docker_tag=$BITBUCKET_TAG
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+cd devops/terraform/staging/${PACKAGE}
+terraform init && terraform validate && terraform fmt
+terraform plan
+terraform apply -auto-approve
+
+if [ $? -ne 0 ]
+then
+    fail
+else
+    success
+fi
