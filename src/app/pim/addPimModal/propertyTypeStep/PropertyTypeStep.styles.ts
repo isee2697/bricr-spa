db0ee5bcf750ff97fc10bf44b9a35da5091838ff
Damian Szczepaniak <damian.szczepaniak@tsh.io>
2020-06-01 07:48:34 +0000

    Merged in fix/BRICR-330-add-property-btns-pos (pull request #165)
    [BRICR-330] spacing fix
    
    Approved-by: Michał Szuścik <michal.szuscik@tsh.io>

--- src/app/pim/addPimModal/propertyTypeStep/PropertyTypeStep.styles.ts
+++ src/app/pim/addPimModal/propertyTypeStep/PropertyTypeStep.styles.ts
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
   selectCard: {
+    paddingRight: theme.spacing(3),
     '& button:hover': {
       '@media (hover: none)': {
         backgroundColor: theme.palette.white.main,
