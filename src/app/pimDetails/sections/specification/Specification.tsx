460da7c4e250b7e34d804b8020985a94a078f73a
Krzysztof Królikowski <krzysztof.krolikowski@tsh.io>
2020-06-25 09:51:05 +0000

    Merged in bugfix/BRICR-695-remove-add-linked-property-button (pull request #278)
    [BRICR-695] remove add linked property button
    
    * [BRICR-695] remove add linked property button
    
    Approved-by: Aleksander Patschek
    Approved-by: Michał Szuścik
    Approved-by: Andreas Otten

--- src/app/pimDetails/sections/specification/Specification.tsx
+++ src/app/pimDetails/sections/specification/Specification.tsx
@@ -1,63 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 
-import { Button, Grid } from 'ui/atoms';
-import { useLocale } from 'hooks';
-import { EditIcon } from 'ui/atoms/icons';
+import { Grid } from 'ui/atoms';
 import { AppRoute } from 'routing/AppRoute.enum';
 import { PimDetailsHeader } from '../../pimDetailsHeader/PimDetailsHeader';
 import { PimDetailsSectionProps } from '../../PimDetails.types';
 
 import { SpecificationGeneralContainer } from './specificationGeneral/SpecificationGeneralContainer';
-import { AddLinkedPropertyModalContainer } from './addLinkedPropertyModal/AddLinkedPropertyModalContainer';
 import { AdvancedContainer } from './advanced/AdvancedContainer';
 import { LinkedPropertyContainer } from './linkedProperty/LinkedPropertyContainer';
 import { InspectionContainer } from './inspection/InspectionContainer';
 
-export const Specification = ({ title, isSidebarVisible, onOpenSidebar }: PimDetailsSectionProps) => {
-  const { formatMessage } = useLocale();
-  const [isLinkedPropertyModalOpen, setLinkedPropertyModalOpen] = useState(false);
-
-  return (
-    <>
-      <Grid xs={12} item>
-        <PimDetailsHeader
-          title={title}
-          isSidebarVisible={isSidebarVisible}
-          onOpenSidebar={onOpenSidebar}
-          action={
-            <Button
-              color="primary"
-              startIcon={<EditIcon color="inherit" />}
-              variant="contained"
-              onClick={() => setLinkedPropertyModalOpen(v => !v)}
-              size="small"
-            >
-              {formatMessage({ id: 'pim_details.specification.add_property_button' })}
-            </Button>
-          }
-        />
-      </Grid>
-      <Switch>
-        <Route
-          default
-          path={`${AppRoute.pimDetails}/specification`}
-          exact
-          render={() => <SpecificationGeneralContainer />}
-        />
-        <Route path={`${AppRoute.pimDetails}/specification/advanced`} exact render={() => <AdvancedContainer />} />
-        <Route
-          path={`${AppRoute.pimDetails}/specification/linked-property`}
-          exact
-          render={() => <LinkedPropertyContainer />}
-        />
-        <Route path={`${AppRoute.pimDetails}/specification/inspection`} exact render={() => <InspectionContainer />} />
-        <Redirect to={`${AppRoute.pimDetails}/specification`} />
-      </Switch>
-      <AddLinkedPropertyModalContainer
-        isOpened={isLinkedPropertyModalOpen}
-        onClose={() => setLinkedPropertyModalOpen(false)}
+export const Specification = ({ title, isSidebarVisible, onOpenSidebar }: PimDetailsSectionProps) => (
+  <>
+    <Grid xs={12} item>
+      <PimDetailsHeader title={title} isSidebarVisible={isSidebarVisible} onOpenSidebar={onOpenSidebar} />
+    </Grid>
+    <Switch>
+      <Route
+        default
+        path={`${AppRoute.pimDetails}/specification`}
+        exact
+        render={() => <SpecificationGeneralContainer />}
       />
-    </>
-  );
-};
+      <Route path={`${AppRoute.pimDetails}/specification/advanced`} exact render={() => <AdvancedContainer />} />
+      <Route
+        path={`${AppRoute.pimDetails}/specification/linked-property`}
+        exact
+        render={() => <LinkedPropertyContainer />}
+      />
+      <Route path={`${AppRoute.pimDetails}/specification/inspection`} exact render={() => <InspectionContainer />} />
+      <Redirect to={`${AppRoute.pimDetails}/specification`} />
+    </Switch>
+  </>
+);
