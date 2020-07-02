3f86c5f36bc0f223f94960becdfe4ecd20aaf33a
Aleksander Patschek <aleksander.patschek@tsh.io>
2020-07-02 10:49:07 +0000

    Merged in fix/BRICR-826-fix-specyfication-dictionaries (pull request #348)
    [BRICR-826] fix specification dictionaries
    
    * [BRICR-826] fix specification dictionaries
    
    Approved-by: Michał Szuścik
    Approved-by: Damian Szczepaniak

--- src/app/pimDetails/sections/specification/dictionaries.tsx
+++ src/app/pimDetails/sections/specification/dictionaries.tsx
@@ -1,455 +1,103 @@
 import React from 'react';
 
 import { SquareIcon } from 'ui/atoms/icons';
+import {
+  ApprovalType,
+  EnergyCharasteristicType,
+  EnergyType,
+  GoodToKnowType,
+  HousingType,
+  InsideType,
+  MaintenanceType,
+  MonumentType,
+  ObligationToProvideInformationType,
+  ParkingFacilities,
+  PollutionType,
+  PropertyRightType,
+  SpecialTagsType,
+  TankType,
+} from 'api/types';
 
-export const energy = [
-  {
-    label: 'dictionaries.specification.energy.A',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A',
-  },
-  {
-    label: 'dictionaries.specification.energy.A1Plus',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A1Plus',
-  },
-  {
-    label: 'dictionaries.specification.energy.A2Plus',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A2Plus',
-  },
-  {
-    label: 'dictionaries.specification.energy.A3Plus',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A3Plus',
-  },
-  {
-    label: 'dictionaries.specification.energy.A4Plus',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A4Plus',
-  },
-  {
-    label: 'dictionaries.specification.energy.A5Plus',
-    icon: <SquareIcon color="inherit" />,
-    value: 'A5Plus',
-  },
-  {
-    label: 'dictionaries.specification.energy.B',
-    icon: <SquareIcon color="inherit" />,
-    value: 'B',
-  },
-  {
-    label: 'dictionaries.specification.energy.C',
-    icon: <SquareIcon color="inherit" />,
-    value: 'C',
-  },
-  {
-    label: 'dictionaries.specification.energy.D',
-    icon: <SquareIcon color="inherit" />,
-    value: 'D',
-  },
-  {
-    label: 'dictionaries.specification.energy.E',
-    icon: <SquareIcon color="inherit" />,
-    value: 'E',
-  },
-  {
-    label: 'dictionaries.specification.energy.F',
-    icon: <SquareIcon color="inherit" />,
-    value: 'F',
-  },
-  {
-    label: 'dictionaries.specification.energy.G',
-    icon: <SquareIcon color="inherit" />,
-    value: 'G',
-  },
-];
+export const energy = Object.keys(EnergyType).map(value => ({
+  label: `dictionaries.specification.energy.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const energyType = [
-  {
-    label: 'dictionaries.specification.energy_type.Beng',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Beng',
-  },
-  {
-    label: 'dictionaries.specification.energy_type.NaturalEnergy',
-    icon: <SquareIcon color="inherit" />,
-    value: 'NaturalEnergy',
-  },
-  {
-    label: 'dictionaries.specification.energy_type.ZeroOnMeter',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ZeroOnMeter',
-  },
-];
+export const energyType = Object.keys(EnergyCharasteristicType).map(value => ({
+  label: `dictionaries.specification.energy_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const approvals = [
-  {
-    label: 'dictionaries.specification.approvals.AdaptedHome',
-    icon: <SquareIcon color="inherit" />,
-    value: 'AdaptedHome',
-  },
-  {
-    label: 'dictionaries.specification.approvals.EnergyPerformanceAdvice',
-    icon: <SquareIcon color="inherit" />,
-    value: 'EnergyPerformanceAdvice',
-  },
-  {
-    label: 'dictionaries.specification.approvals.PoliceMark',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PoliceMark',
-  },
-  {
-    label: 'dictionaries.specification.approvals.GeneralHomeApproval',
-    icon: <SquareIcon color="inherit" />,
-    value: 'GeneralHomeApproval',
-  },
-  {
-    label: 'dictionaries.specification.approvals.GIWGuaranteeCertificate',
-    icon: <SquareIcon color="inherit" />,
-    value: 'GIWGuaranteeCertificate',
-  },
-  {
-    label: 'dictionaries.specification.approvals.SWKGuaranteeCertificate',
-    icon: <SquareIcon color="inherit" />,
-    value: 'SWKGuaranteeCertificate',
-  },
-  {
-    label: 'dictionaries.specification.approvals.ArchitecturalInspection',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ArchitecturalInspection',
-  },
-  {
-    label: 'dictionaries.specification.approvals.HouseGuarantee',
-    icon: <SquareIcon color="inherit" />,
-    value: 'HouseGuarantee',
-  },
-  {
-    label: 'dictionaries.specification.approvals.HousingGuaranteeCertificate',
-    icon: <SquareIcon color="inherit" />,
-    value: 'HousingGuaranteeCertificate',
-  },
-  {
-    label: 'dictionaries.specification.approvals.FireSafety',
-    icon: <SquareIcon color="inherit" />,
-    value: 'FireSafety',
-  },
-  {
-    label: 'dictionaries.specification.approvals.KIWASafeLiving',
-    icon: <SquareIcon color="inherit" />,
-    value: 'KIWASafeLiving',
-  },
-  {
-    label: 'dictionaries.specification.approvals.QualityMarkCertificate',
-    icon: <SquareIcon color="inherit" />,
-    value: 'QualityMarkCertificate',
-  },
-];
+export const approvals = Object.keys(ApprovalType).map(value => ({
+  label: `dictionaries.specification.approvals.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const obligation = [
-  {
-    label: 'dictionaries.specification.obligation.Boot',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Boot',
-  },
-  {
-    label: 'dictionaries.specification.obligation.BAndWLetter',
-    icon: <SquareIcon color="inherit" />,
-    value: 'BAndWLetter',
-  },
-  {
-    label: 'dictionaries.specification.obligation.SoilPollution',
-    icon: <SquareIcon color="inherit" />,
-    value: 'SoilPollution',
-  },
-  {
-    label: 'dictionaries.specification.obligation.Asbestos',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Asbestos',
-  },
-  {
-    label: 'dictionaries.specification.obligation.OwnSake',
-    icon: <SquareIcon color="inherit" />,
-    value: 'OwnSake',
-  },
-];
+export const obligation = Object.keys(ObligationToProvideInformationType).map(value => ({
+  label: `dictionaries.specification.obligation.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const parking = [
-  {
-    label: 'dictionaries.specification.advanced_parking.PaidParking',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PaidParking',
-  },
-  {
-    label: 'dictionaries.specification.advanced_parking.OnSite',
-    icon: <SquareIcon color="inherit" />,
-    value: 'OnSite',
-  },
-  {
-    label: 'dictionaries.specification.advanced_parking.ParkingGarage',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ParkingGarage',
-  },
-  {
-    label: 'dictionaries.specification.advanced_parking.OnClosedTerrain',
-    icon: <SquareIcon color="inherit" />,
-    value: 'OnClosedTerrain',
-  },
-  {
-    label: 'dictionaries.specification.advanced_parking.PublicParking',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PublicParking',
-  },
-  {
-    label: 'dictionaries.specification.advanced_parking.ParkingPermits',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ParkingPermits',
-  },
-];
+export const parking = Object.keys(ParkingFacilities).map(value => ({
+  label: `dictionaries.specification.advanced_parking.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const advancedMonument = [
-  {
-    label: 'dictionaries.specification.monument_type.Heritage',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Heritage',
-  },
-  {
-    label: 'dictionaries.specification.monument_type.MonumentalProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'MonumentalProperty',
-  },
-  {
-    label: 'dictionaries.specification.monument_type.ProtectedCityOrVillageView',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ProtectedCityOrVillageView',
-  },
-  {
-    label: 'dictionaries.specification.monument_type.NationalMonument',
-    icon: <SquareIcon color="inherit" />,
-    value: 'NationalMonument',
-  },
-  {
-    label: 'dictionaries.specification.monument_type.MunciapalMonument',
-    icon: <SquareIcon color="inherit" />,
-    value: 'MunciapalMonument',
-  },
-];
+export const advancedMonument = Object.keys(MonumentType).map(value => ({
+  label: `dictionaries.specification.monument_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const advancedInside = [
-  {
-    label: 'dictionaries.specification.inside_type.Furnished',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Furnished',
-  },
-  {
-    label: 'dictionaries.specification.inside_type.Upholstered',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Upholstered',
-  },
-  {
-    label: 'dictionaries.specification.inside_type.PartlyUpholstered',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PartlyUpholstered',
-  },
-];
+export const advancedInside = Object.keys(InsideType).map(value => ({
+  label: `dictionaries.specification.inside_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const advancedOptions = [
-  {
-    label: 'dictionaries.specification.housing_type.PartiallyRented',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PartiallyRented',
-  },
-  {
-    label: 'dictionaries.specification.housing_type.DoubleOccupancyAvailable',
-    icon: <SquareIcon color="inherit" />,
-    value: 'DoubleOccupancyAvailable',
-  },
-  {
-    label: 'dictionaries.specification.housing_type.DoubleOccupancyPossible',
-    icon: <SquareIcon color="inherit" />,
-    value: 'DoubleOccupancyPossible',
-  },
-  {
-    label: 'dictionaries.specification.housing_type.AccessibleToDisabledPeople',
-    icon: <SquareIcon color="inherit" />,
-    value: 'AccessibleToDisabledPeople',
-  },
-  {
-    label: 'dictionaries.specification.housing_type.AccessibleToTheElderly',
-    icon: <SquareIcon color="inherit" />,
-    value: 'AccessibleToTheElderly',
-  },
-];
+export const advancedOptions = Object.keys(HousingType).map(value => ({
+  label: `dictionaries.specification.housing_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const advancedTags = [
-  {
-    label: 'dictionaries.specification.special_type.DemolitionProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'DemolitionProperty',
-  },
-  {
-    label: 'dictionaries.specification.special_type.DoItYourselfProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'DoItYourselfProperty',
-  },
-  {
-    label: 'dictionaries.specification.special_type.SeniorsProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'SeniorsProperty',
-  },
-  {
-    label: 'dictionaries.specification.special_type.RuralLiving',
-    icon: <SquareIcon color="inherit" />,
-    value: 'RuralLiving',
-  },
-  {
-    label: 'dictionaries.specification.special_type.The30',
-    icon: <SquareIcon color="inherit" />,
-    value: 'The30',
-  },
-  {
-    label: 'dictionaries.specification.special_type.LivingNearTheWater',
-    icon: <SquareIcon color="inherit" />,
-    value: 'LivingNearTheWater',
-  },
-  {
-    label: 'dictionaries.specification.special_type.ExclusiveProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ExclusiveProperty',
-  },
-  {
-    label: 'dictionaries.specification.special_type.Turnkey',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Turnkey',
-  },
-  {
-    label: 'dictionaries.specification.special_type.ReadyToUse',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ReadyToUse',
-  },
-  {
-    label: 'dictionaries.specification.special_type.StarterProperty',
-    icon: <SquareIcon color="inherit" />,
-    value: 'StarterProperty',
-  },
-];
+export const advancedTags = Object.keys(SpecialTagsType).map(value => ({
+  label: `dictionaries.specification.special_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const advancedProperty = [
-  {
-    label: 'dictionaries.specification.business_type.BPRights',
-    icon: <SquareIcon color="inherit" />,
-    value: 'BPRights',
-  },
-  {
-    label: 'dictionaries.specification.business_type.Easements',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Easements',
-  },
-  {
-    label: 'dictionaries.specification.business_type.RightOfSuperficies',
-    icon: <SquareIcon color="inherit" />,
-    value: 'RightOfSuperficies',
-  },
-  {
-    label: 'dictionaries.specification.business_type.Cooperative',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Cooperative',
-  },
-  {
-    label: 'dictionaries.specification.business_type.Horizontal',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Horizontal',
-  },
-];
+export const advancedProperty = Object.keys(PropertyRightType).map(value => ({
+  label: `dictionaries.specification.business_type.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const goodToKnow = [
-  {
-    label: 'dictionaries.specification.good_to_know.MaintenancePlan',
-    icon: <SquareIcon color="inherit" />,
-    value: 'MaintenancePlan',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.ReserveFund',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ReserveFund',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.HomeInsurance',
-    icon: <SquareIcon color="inherit" />,
-    value: 'HomeInsurance',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.PeriodicContribution',
-    icon: <SquareIcon color="inherit" />,
-    value: 'PeriodicContribution',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.Meeting',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Meeting',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.RegistrationChamberOfCommerce',
-    icon: <SquareIcon color="inherit" />,
-    value: 'RegistrationChamberOfCommerce',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.ChecklistPresent',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ChecklistPresent',
-  },
-  {
-    label: 'dictionaries.specification.good_to_know.Active',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Active',
-  },
-];
+export const goodToKnow = Object.keys(GoodToKnowType).map(value => ({
+  label: `dictionaries.specification.good_to_know.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const inspectionTank = [
-  {
-    label: 'dictionaries.inspection_tanks.Septic',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Septic',
-  },
-  {
-    label: 'dictionaries.inspection_tanks.Oil',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Oil',
-  },
-  {
-    label: 'dictionaries.inspection_tanks.Underground',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Underground',
-  },
-];
+export const inspectionTank = Object.keys(TankType).map(value => ({
+  label: `dictionaries.inspection_tanks.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const inspectionPollution = [
-  {
-    label: 'dictionaries.inspection_pollution.Asbestos',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Asbestos',
-  },
-  {
-    label: 'dictionaries.inspection_pollution.Soil',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Soil',
-  },
-];
+export const inspectionPollution = Object.keys(PollutionType).map(value => ({
+  label: `dictionaries.inspection_pollution.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
 
-export const inspectionMaintenance = [
-  {
-    label: 'dictionaries.inspection_maintenance.Paintwork',
-    icon: <SquareIcon color="inherit" />,
-    value: 'Paintwork',
-  },
-  {
-    label: 'dictionaries.inspection_maintenance.ElectricityConnections',
-    icon: <SquareIcon color="inherit" />,
-    value: 'ElectricityConnections',
-  },
-  {
-    label: 'dictionaries.inspection_maintenance.WindowFrames',
-    icon: <SquareIcon color="inherit" />,
-    value: 'WindowFrames',
-  },
-];
+export const inspectionMaintenance = Object.keys(MaintenanceType).map(value => ({
+  label: `dictionaries.inspection_maintenance.${value}`,
+  icon: <SquareIcon color="inherit" />,
+  value,
+}));
