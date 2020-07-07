11a129130099f8e15afed34b76d61cacee85efbd
Piotr Ferens <piotr.ferens@tsh.io>
2020-07-07 12:28:07 +0000

    Merged in bugfix/BRICR-864-fix-highlight-on-touch-device (pull request #375)
    [BRICR-864] fix highlight items in menu on touch devices
    
    * [BRICR-864] fix highlight items in menu on touch devices
    
    Approved-by: Aleksander Patschek
    Approved-by: Michał Szuścik
    Approved-by: Damian Szczepaniak

--- src/ui/atoms/sideMenuItem/SideMenuItem.styles.ts
+++ src/ui/atoms/sideMenuItem/SideMenuItem.styles.ts
@@ -1,4 +1,17 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const hoverAndSelectedStyles = (theme: Theme) => ({
+  '& > h3, & a': {
+    color: theme.palette.black.main,
+  },
+  '& path': {
+    fill: theme.palette.primary.main,
+  },
+  '&::after': {
+    borderBottomColor: theme.palette.primary.main,
+    boxShadow: theme.shadows[2],
+  },
+});
 
 export const useStyles = makeStyles(theme => ({
   item: {
@@ -15,17 +28,14 @@ export const useStyles = makeStyles(theme => ({
       borderBottomColor: theme.palette.gray.light,
       borderBottomStyle: 'solid',
     },
-    '&.Mui-selected, &:hover, &.Mui-selected:hover': {
+    '&.Mui-selected': {
       backgroundColor: 'transparent',
-      '& > h3, & a': {
-        color: theme.palette.black.main,
-      },
-      '& path': {
-        fill: theme.palette.primary.main,
-      },
-      '&::after': {
-        borderBottomColor: theme.palette.primary.main,
-        boxShadow: theme.shadows[2],
+      ...hoverAndSelectedStyles(theme),
+    },
+    '&:hover, &.Mui-selected:hover': {
+      backgroundColor: 'transparent',
+      '@media (hover: hover)': {
+        ...hoverAndSelectedStyles(theme),
       },
     },
   },
