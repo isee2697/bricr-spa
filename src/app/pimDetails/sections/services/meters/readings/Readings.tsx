8a135c35330b11b1d925bc2989cc27677b26424c
Piotr Ferens <piotr.ferens@tsh.io>
2020-07-08 06:01:25 +0000

    Merged in bugfix/BRICR-872-fix-label-typography (pull request #377)
    [BRICR-872] fix label typography
    
    * [BRICR-872] fix label typography
    
    Approved-by: Aleksander Patschek
    Approved-by: Michał Szuścik
    Approved-by: Damian Szczepaniak

--- src/app/pimDetails/sections/services/meters/readings/Readings.tsx
+++ src/app/pimDetails/sections/services/meters/readings/Readings.tsx
@@ -4,7 +4,6 @@ import classNames from 'classnames';
 import { useParams } from 'react-router-dom';
 
 import { useLocale, useToogleOnNewlyCreated } from 'hooks';
-import { Box } from 'ui/atoms';
 import { LinkedPerson } from 'ui/molecules';
 import { AutosaveForm, FormSubSection } from 'ui/organisms';
 import { GenericField, DatePickerField } from 'form/fields';
@@ -35,42 +34,36 @@ export const Readings = ({ readings, editing, linkedPerson, onSave, isMeterAdded
           >
             <Grid container spacing={1}>
               <Grid item xs={isElectricity ? 4 : 6}>
-                <Box display="flex" alignItems="flex-end" height="100%">
-                  <GenericField
-                    size="medium"
-                    label="pim_details.services.meter.value"
-                    placeholder="pim_details.services.meter.value_placeholder"
-                    type="number"
-                    name="value"
-                    id={`value.${reading.id}`}
-                    disabled={!editing}
-                  />
-                </Box>
+                <GenericField
+                  size="medium"
+                  label="pim_details.services.meter.value"
+                  placeholder="pim_details.services.meter.value_placeholder"
+                  type="number"
+                  name="value"
+                  id={`value.${reading.id}`}
+                  disabled={!editing}
+                />
               </Grid>
               <Grid item xs={isElectricity ? 4 : 6}>
-                <Box display="flex" alignItems="flex-end" height="100%">
-                  <DatePickerField
-                    name="dateOfReading"
-                    id={`date.${reading.id}`}
-                    label="pim_details.services.meter.reading_date"
-                    placeholder="pim_details.services.meter.id_placeholder"
-                    size="medium"
-                    disabled={!editing}
-                  />
-                </Box>
+                <DatePickerField
+                  name="dateOfReading"
+                  id={`date.${reading.id}`}
+                  label="pim_details.services.meter.reading_date"
+                  placeholder="pim_details.services.meter.id_placeholder"
+                  size="medium"
+                  disabled={!editing}
+                />
               </Grid>
               {isElectricity && (
                 <Grid item xs={4}>
-                  <Box display="flex" alignItems="flex-end" height="100%">
-                    <GenericField
-                      size="medium"
-                      label="pim_details.services.meter.id"
-                      placeholder="pim_details.services.meter.id"
-                      name="feedInId"
-                      id={`feed-in.${reading.id}`}
-                      disabled={!editing}
-                    />
-                  </Box>
+                  <GenericField
+                    size="medium"
+                    label="pim_details.services.meter.id"
+                    placeholder="pim_details.services.meter.id"
+                    name="feedInId"
+                    id={`feed-in.${reading.id}`}
+                    disabled={!editing}
+                  />
                 </Grid>
               )}
               <Grid item xs={12}>
