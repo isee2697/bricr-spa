4543b13f2747ddceb5c93b8ff2845f94f5719321
Aleksander Patschek <aleksander.patschek@tsh.io>
2020-07-30 12:11:37 +0000

    Merged in fix/BRICR-955-object-types-list-filter (pull request #473)
    [BRICR-955] fix filters in object types list
    
    * [BRICR-955] fix filters in object types list
    
    Approved-by: Michał Szuścik

--- src/app/projectDetails/sections/objectTypes/useObjectTypesSorting/useObjectTypesSorting.ts
+++ src/app/projectDetails/sections/objectTypes/useObjectTypesSorting/useObjectTypesSorting.ts
@@ -5,14 +5,10 @@ import { useSorting } from 'hooks';
 const SORTING_DATA: { [key: string]: { sortColumn: string; sortDirection: SortDirection } } = {
   last_created: { sortColumn: 'dateCreated', sortDirection: SortDirection.Desc },
   first_created: { sortColumn: 'dateCreated', sortDirection: SortDirection.Asc },
-  lowest_rent_price_from: { sortColumn: 'rentPriceFrom', sortDirection: SortDirection.Asc },
-  highest_rent_price_from: { sortColumn: 'rentPriceFrom', sortDirection: SortDirection.Desc },
-  lowest_rent_price_to: { sortColumn: 'rentPriceTo', sortDirection: SortDirection.Asc },
-  highest_rent_price_to: { sortColumn: 'rentPriceTo', sortDirection: SortDirection.Desc },
-  lowest_sale_price_from: { sortColumn: 'salePriceFrom', sortDirection: SortDirection.Asc },
-  highest_sale_price_from: { sortColumn: 'salePriceFrom', sortDirection: SortDirection.Desc },
-  lowest_sale_price_to: { sortColumn: 'salePriceTo', sortDirection: SortDirection.Asc },
-  highest_sale_price_to: { sortColumn: 'salePriceTo', sortDirection: SortDirection.Desc },
+  lowest_rent_price: { sortColumn: 'rentPriceFrom', sortDirection: SortDirection.Asc },
+  highest_rent_price: { sortColumn: 'rentPriceFrom', sortDirection: SortDirection.Desc },
+  lowest_sale_price: { sortColumn: 'salePriceFrom', sortDirection: SortDirection.Asc },
+  highest_sale_price: { sortColumn: 'salePriceFrom', sortDirection: SortDirection.Desc },
 };
 
 export const useObjectTypesSorting = () => useSorting(SORTING_DATA);
