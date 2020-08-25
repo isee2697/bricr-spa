d3fb0a65e34d2d1c9c5d42cc524b43109d832189
Andreas Otten <andreas@cuniceyes.com>
2020-08-25 21:01:36 +0200

    fix to check if file is actually blob

--- src/ui/organisms/uploadModal/UploadModal.helpers.ts
+++ src/ui/organisms/uploadModal/UploadModal.helpers.ts
@@ -13,7 +13,7 @@ export const readFileAsync = (file: File, asArrayBuffer = false): Promise<string
 
     if (asArrayBuffer) {
       reader.readAsArrayBuffer(file);
-    } else {
+    } else if (file instanceof Blob) {
       reader.readAsDataURL(file);
     }
   });
