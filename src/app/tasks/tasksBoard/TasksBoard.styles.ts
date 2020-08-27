bb9d8f5707430e9c6e145084f38221e6b7a89ddd
bigcodeben <dempsterbill44@gmail.com>
2020-08-27 18:24:38 -0400

    UL-1246 Empty task board

--- src/app/tasks/tasksBoard/TasksBoard.styles.ts
+++ src/app/tasks/tasksBoard/TasksBoard.styles.ts
@@ -6,4 +6,37 @@ export const useStyles = makeStyles(theme => ({
     borderBottomRightRadius: 0,
     minHeight: '70vh',
   },
+  tasksStatus: {},
+  tasksStatusLogo: {
+    fontSize: 40,
+  },
+  tasksStatusMessage: {
+    fontSize: 12,
+    fontWeight: theme.typography.fontWeightBold,
+  },
+  sortIcon: {
+    marginLeft: theme.spacing(3),
+  },
+  taskItemsColumn: {
+    backgroundColor: theme.palette.gray.light,
+    borderRadius: 8,
+    height: '100%',
+  },
+  noPadding: {
+    padding: 0,
+  },
+  flexRow: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  flexColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  flexGrowZero: {
+    flex: 0,
+  },
+  flexGrowOne: {
+    flex: 1,
+  },
 }));
--- src/app/tasks/tasksBoard/TasksBoard.tsx
+++ src/app/tasks/tasksBoard/TasksBoard.tsx
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
+import clsx from 'classnames';
 import { useTheme } from '@material-ui/core';
 
-import { Box, Grid, Typography, Card, CardHeader, CardContent, Alert, Tabs, Tab, Scrollable } from 'ui/atoms';
+import { SimpleSearch } from 'ui/molecules';
+import {
+  Box,
+  Grid,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Alert,
+  Tabs,
+  Tab,
+  Scrollable,
+  IconButton,
+} from 'ui/atoms';
+import { ListIcon } from 'ui/atoms/icons/list/ListIcon';
+import { MenuIcon } from 'ui/atoms/icons/menu/MenuIcon';
+import { SwimlaneIcon } from 'ui/atoms/icons/swimlane/SwimlaneIcon';
+import { ManageIcon } from 'ui/atoms/icons/manage/ManageIcon';
 import { useLocale } from 'hooks/useLocale/useLocale';
 
 import { useStyles } from './TasksBoard.styles';
@@ -11,9 +29,10 @@ export const TasksBoard = () => {
   const theme = useTheme();
   const { formatMessage } = useLocale();
   const [activeTab, setActiveTab] = useState(0);
+  const [value, setValue] = useState('');
 
   return (
-    <Card className={classes.root}>
+    <Card className={clsx(classes.root, classes.flexColumn)}>
       <Tabs
         value={activeTab}
         onChange={(e, value) => setActiveTab(value)}
@@ -26,19 +45,104 @@ export const TasksBoard = () => {
         <Tab label={formatMessage({ id: 'tasks.future' })} />
         <Tab label={formatMessage({ id: 'tasks.overdue' })} />
       </Tabs>
-      <CardContent>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            List 1
+      <CardContent className={clsx(classes.flexColumn, classes.flexGrowOne)}>
+        <Grid container spacing={2} className={classes.flexGrowOne} direction="column">
+          <Grid item xs={12} container alignItems="center" justify="space-between" className={classes.flexGrowZero}>
+            <Grid item xs={3}>
+              <Grid container direction="column" className={classes.tasksStatus} alignItems="center">
+                <Grid item className={classes.tasksStatusLogo}>
+                  🎉
+                </Grid>
+                <Grid item className={classes.tasksStatusMessage}>
+                  Ta-da!
+                </Grid>
+                <Grid item className={classes.tasksStatusMessage}>
+                  You are up to date
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid container>
+                <SimpleSearch onChange={v => setValue(v.currentTarget.value)} value={value} />
+                <IconButton classes={{ root: classes.sortIcon }}>
+                  <SwimlaneIcon color="inherit" />
+                </IconButton>
+                <IconButton classes={{ root: classes.sortIcon }}>
+                  <ListIcon color="inherit" />
+                </IconButton>
+                <IconButton classes={{ root: classes.sortIcon }}>
+                  <ManageIcon color="inherit" />
+                </IconButton>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            List 2
-          </Grid>
-          <Grid item xs={3}>
-            List 3
-          </Grid>
-          <Grid item xs={3}>
-            List 4
+          <Grid item xs={12} className={clsx(classes.flexGrowOne, classes.flexRow)}>
+            <Grid container spacing={2} className={classes.flexRow}>
+              <Grid item xs={3}>
+                <Grid container spacing={1} className={classes.taskItemsColumn}>
+                  <Grid item className={classes.flexGrowOne}>
+                    <Grid container justify="space-between">
+                      <Grid item className={classes.flexGrowOne}>
+                        ⏱ To do
+                      </Grid>
+                      <Grid item>
+                        <IconButton className={classes.noPadding}>
+                          <MenuIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={3}>
+                <Grid container spacing={1} className={classes.taskItemsColumn}>
+                  <Grid item className={classes.flexGrowOne}>
+                    <Grid container justify="space-between">
+                      <Grid item className={classes.flexGrowOne}>
+                        🔥 In progress
+                      </Grid>
+                      <Grid item>
+                        <IconButton className={classes.noPadding}>
+                          <MenuIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={3}>
+                <Grid container spacing={1} className={classes.taskItemsColumn}>
+                  <Grid item className={classes.flexGrowOne}>
+                    <Grid container justify="space-between">
+                      <Grid item className={classes.flexGrowOne}>
+                        ⛔️ Blocked
+                      </Grid>
+                      <Grid item>
+                        <IconButton className={classes.noPadding}>
+                          <MenuIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={3}>
+                <Grid container spacing={1} className={classes.taskItemsColumn}>
+                  <Grid item className={classes.flexGrowOne}>
+                    <Grid container justify="space-between">
+                      <Grid item className={classes.flexGrowOne}>
+                        ✅ Done
+                      </Grid>
+                      <Grid item>
+                        <IconButton className={classes.noPadding}>
+                          <MenuIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </CardContent>
