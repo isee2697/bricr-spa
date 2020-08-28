a0035708219ba74a380ebd8afd77b795de611876
isee2697 <isee2697@outlook.com>
2020-08-28 08:12:53 +0000

    NotificationMenu.styles.ts edited online with Bitbucket

--- src/ui/templates/dashboard/notificationMenu/NotificationMenu.styles.ts
+++ src/ui/templates/dashboard/notificationMenu/NotificationMenu.styles.ts
@@ -1,7 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
-  notification: {},
+  notification: {
+    cursor: 'pointer',
+  },
   activeNotificationIcon: {
     backgroundColor: `${theme.palette.gray.main} !important`,
     '& .MuiSvgIcon-root path': {
