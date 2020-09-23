d838d98258efd5505a767e1925d73c4e618f2032
Elton <elton@trivetechnology.com>
2020-09-23 13:01:24 +0200

    updated the filter sidemenu

--- src/ui/molecules/filters/FilterSideMenu/FilterSideMenu.tsx
+++ src/ui/molecules/filters/FilterSideMenu/FilterSideMenu.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { Tabs, Tab } from 'ui/atoms';
 import { HomeIcon } from 'ui/atoms/icons/home/HomeIcon';
 import { BuildingIcon } from 'ui/atoms/icons/building/BuildingIcon';
+import { filtersTypes } from '../Filters.types';
 
 import { FilterSidenavProps } from './FilterSideMenu.types';
 
 export const FilterSideMenu = ({ filters }: FilterSidenavProps) => {
   return (
     <Tabs value={0} indicatorColor="primary" textColor="primary" orientation="vertical">
-      {filters.map(item => {
+      {filters.map((item: filtersTypes) => {
         return <Tab key={item.key} label={item.key} icon={<HomeIcon color="inherit" />} />;
       })}
     </Tabs>
