ab5e572673d37ce1b5180f1891c44e42dd419f5b
isee2697 <isee2697@outlook.com>
2020-09-24 10:44:49 +0200

    added config to run with args so pipeline should work?

--- scripts/get_translations.js
+++ scripts/get_translations.js
@@ -4,13 +4,43 @@ const fs = require('fs');
 require('dotenv').config();
 
 const directoryPath = path.join(__dirname, '..', 'src', 'i18n', 'data');
-const apiParam = `?access_token=${process.env.PHRASE_ACCESS_TOKEN}`;
-const baseUrl = `https://api.phrase.com/v2/projects/${process.env.REACT_APP_PHRASE_PROJECT}/locales`
+const apiParam = `?access_token=:accessToken`;
+const baseUrl = `https://api.phrase.com/v2/projects/:projectID/locales`
 
-console.log(process);
-const saveLocaleFunction = async (id, name) => {
-    const url = `${baseUrl}/${id}/download${apiParam}&file_format=react_simple_json`;
 
+const getVariables = () => {
+    let accessToken = process.env.PHRASE_ACCESS_TOKEN;
+    let projectID = process.env.REACT_APP_PHRASE_PROJECT;
+    if (!accessToken && process.argv.length > 0) {
+        const argvAccessKeyMatchString = '--accessToken=';
+        const accessKey = process.argv.find(item => item.includes(argvAccessKeyMatchString));
+
+        if (!!accessKey) {
+            accessToken = accessKey.replace(argvAccessKeyMatchString, '');
+        } else {
+            throw Error('No AccesToken found');
+        }
+    }
+
+    if (!projectID && process.argv.length > 0) {
+        const argvProjectMatchString = '--projectID=';
+        const projectIDArg = process.argv.find(item => item.includes(argvProjectMatchString));
+
+        if (!!projectIDArg) {
+            projectID = projectIDArg.replace(argvProjectMatchString, '');
+        } else {
+            throw new Error('No Project ID found');
+        }
+    }
+
+    return { projectID, accessToken }
+}
+
+const saveLocaleFunction = async (id, name, variables) => {
+
+    const url = `${baseUrl.replace(':projectID', variables.projectID)}/${id}/download${apiParam.replace(':accessToken', variables.accessToken)}&file_format=react_simple_json`;
+    // const url = baseUrl.replace(':projectID', variables.projectID) `${baseUrl}/${id}/download${apiParam}&file_format=react_simple_json`;
+    console.log(url);
     try {
         await fetch(url).then(res => res.json()).then(body => {
             const filePath = path.join(directoryPath, `${name}.json`);
@@ -24,13 +54,13 @@ const saveLocaleFunction = async (id, name) => {
 
 const getLocalesList = async () => {
     try {
-        if(!process.env.PHRASE_ACCESS_TOKEN || !process.env.REACT_APP_PHRASE_PROJECT) {
-            throw Error('Environment not set');
-        }
+        const variables = getVariables();
 
-        await fetch(`${baseUrl}${apiParam}`).then(res => res.json()).then(body => {
+        const url = `${baseUrl.replace(':projectID', variables.projectID)}${apiParam.replace(':accessToken', variables.accessToken)}`;
+
+        await fetch(url).then(res => res.json()).then(body => {
             if (typeof body === 'object') {
-                Object.values(body).forEach(value => saveLocaleFunction(value.id, value.name));
+                Object.values(body).forEach(value => saveLocaleFunction(value.id, value.name, variables));
             }
         });
     } catch (e) {
