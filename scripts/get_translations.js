a05c23fec8d49c483c54beacefc5e02494c9c898
isee2697 <isee2697@outlook.com>
2020-09-29 10:52:33 +0200

    Added to pipeline with node ocmmand

--- scripts/get_translations.js
+++ scripts/get_translations.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const directoryPath = path.join(__dirname, '..', 'src', 'i18n', 'data');
 const apiParam = '?access_token=:accessToken';
 const baseUrl = 'https://api.phrase.com/v2/projects/:projectID/locales';
-
+const encoding = 'UTF-8';
 
 const getVariables = () => {
     let accessToken = process.env.PHRASE_ACCESS_TOKEN;
@@ -36,18 +36,20 @@ const getVariables = () => {
     return { projectID, accessToken }
 }
 
-const saveLocaleFunction = async (id, name, variables) => {
-    const url = `${baseUrl.replace(':projectID', variables.projectID)}/${id}/download${apiParam.replace(':accessToken', variables.accessToken)}&file_format=react_simple_json`;
+const saveLocaleFunction = async (value, variables) => {
+    const downloadParams = `${apiParam.replace(':accessToken', variables.accessToken)}&file_format=react_simple_json&encoding=${encoding}`;
+    const urlWithIds = `${baseUrl.replace(':projectID', variables.projectID)}/${value.id}/download`;
+    const url = `${urlWithIds}${downloadParams}`;
 
-    try {
-        await fetch(url).then(res => res.json()).then(body => {
-            const filePath = path.join(directoryPath, `${name}.json`);
-            fs.writeFileSync(filePath, JSON.stringify(body, null, 2), {encoding: 'utf8', flag: 'w'});
-            console.log(`Succesfully generated language: ${name}`);
-        });
-    } catch (e) {
-        console.error(`Retrieving locale: ${name} with id: ${id} failed`, e);
-    }
+    await fetch(url).then(
+        res => res.status === 200 ? res.json() : console.error('Failed to retrieve:'. res.message())
+    ).then(body => {
+        const filePath = path.join(directoryPath, `${value.code}.json`);
+        fs.writeFileSync(filePath, JSON.stringify(body, null, 2), { encoding, flag: 'w'});
+        console.log(`Succesfully generated language: ${value.name}`);
+    }).catch((e) => {
+        console.log(`Retrieving locale: ${name} with id: ${id} failed`, e);
+    });
 }
 
 const getLocalesList = async () => {
@@ -56,13 +58,17 @@ const getLocalesList = async () => {
 
         const url = `${baseUrl.replace(':projectID', variables.projectID)}${apiParam.replace(':accessToken', variables.accessToken)}`;
 
-        await fetch(url).then(res => res.json()).then(body => {
-            if (typeof body === 'object') {
-                Object.values(body).forEach(value => saveLocaleFunction(value.id, value.name, variables));
-            }
-        });
+        await fetch(url).then(
+            res => res.status === 200 ? res.json() : console.error(res.message()) )
+            .then(body => {
+                if (typeof body === 'object') {
+                    Object.values(body).forEach(value => saveLocaleFunction(value, variables));
+                }
+            }).catch((e) => {
+                console.error('failed to retrieve locales', e);
+            });
     } catch (e) {
-        console.error('Something went wrong fetching the locales', e);
+        console.error('Something went wrong getting the variables', e);
     }
 }
 
