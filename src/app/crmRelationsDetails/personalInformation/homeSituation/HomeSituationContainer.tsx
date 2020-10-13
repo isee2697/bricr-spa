f0d58898f7de40acb972e4b22f4cd3356835aece
Kevin Brendel <kevin@cubiceyes.com>
2020-10-13 13:30:45 +0200

    eslint + typo

--- src/app/crmRelationsDetails/personalInformation/homeSituation/HomeSituationContainer.tsx
+++ src/app/crmRelationsDetails/personalInformation/homeSituation/HomeSituationContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import { UpdateCrmHomeSituationInput, useGetCrmHomeSituationQuery } from 'api/types';
 import { useUpdateCrmHomeSituationMutation } from 'api/types';
 
--- src/app/crmRelationsDetails/personalInformation/homeSituation/currentSituation/dictionaries.tsx
+++ src/app/crmRelationsDetails/personalInformation/homeSituation/currentSituation/dictionaries.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { HomeIcon, SquareIcon } from 'ui/atoms/icons';
 
 export const currentHomes1 = [
