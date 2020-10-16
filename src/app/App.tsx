3e3dd66e9b2e7a9d6e6aa007fd542491189ae3a9
isee2697 <isee2697@outlook.com>
2020-10-16 00:54:15 -0400

    UL-1021 Add timeline modal dialog

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,10 +5,13 @@ import { Snackbar } from 'ui/molecules';
 import { Intercom } from 'app/intercom/Intercom';
 
 import { AddPimModalContainer } from './shared/addPimModal/AddPimModalContainer';
+import { AddCrmTimelineModalContainer } from './shared/addCrmTimelineModal/AddCrmTimelineModalContainer';
+
 export const App = () => (
   <>
     <AppRoutes />
     <AddPimModalContainer />
+    <AddCrmTimelineModalContainer />
     <Snackbar />
     <Intercom />
   </>
