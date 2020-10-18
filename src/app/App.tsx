5ff5d7b2d1cfc1c3a59bc3ffde7dc392316a49ea
isee2697 <isee2697@outlook.com>
2020-10-18 14:49:49 -0400

    UL-1005 Link to profile and partner modal dialogs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,11 +6,16 @@ import { Intercom } from 'app/intercom/Intercom';
 
 import { AddPimModalContainer } from './shared/addPimModal/AddPimModalContainer';
 import { AddCrmRelationModalContainer } from './shared/addCrmRelationModal/AddCrmRelationModalContainer';
+import { LinkPartnerModalContainer } from './shared/linkPartnerModal/LinkPartnerModalContainer';
+import { LinkProfileModalContainer } from './shared/linkProfileModal/LinkProfileModalContainer';
+
 export const App = () => (
   <>
     <AppRoutes />
     <AddPimModalContainer />
     <AddCrmRelationModalContainer />
+    <LinkPartnerModalContainer />
+    <LinkProfileModalContainer />
     <Snackbar />
     <Intercom />
   </>
