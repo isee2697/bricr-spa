5d1cc7ded4329f1fb44c721b2de4106e0a9f994e
Kevin Brendel <kevin@cubiceyes.com>
2020-10-21 13:23:42 +0200

    connected create crm modal to backend

--- src/app/shared/addCrmRelationModal/AddCrmRelationModal.types.ts
+++ src/app/shared/addCrmRelationModal/AddCrmRelationModal.types.ts
@@ -1,13 +1,15 @@
+import { CreateCrmInput } from 'api/types';
+
 export type AddCrmRelationModalProps = {
   isOpened: boolean;
   onClose: VoidFunction;
-  onCreateNewRelation: VoidFunction;
+  onCreateNewRelation: (input: CreateCrmInput) => Promise<void>;
   onRequestBricrData: VoidFunction;
 };
 
 export type AddCrmRelationStepProps = {
   handleGoTo: (step: number) => void;
-  onCreateNewRelation: () => void;
+  onCreateNewRelation: (input: CreateCrmInput) => Promise<void>;
   onRequestBricrData: () => void;
   onClose: () => void;
 };
