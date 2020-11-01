84906814134410b0705a183964038e29a37fb5b2
pgeoff <pgeoff057@gmail.com>
2020-11-01 09:52:04 -0800

    UL-1514 Allocate result list initial UI

--- src/app/projectDetails/sections/allocateResults/list/List.tsx
+++ src/app/projectDetails/sections/allocateResults/list/List.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Page } from 'ui/templates';
-import { Card, CardContent, Typography } from 'ui/atoms';
+import { Box, Card, CardContent, CardHeader, IconButton, Tab, Tabs, Typography } from 'ui/atoms';
 import { InfoSection } from 'ui/molecules';
 import { useLocale } from 'hooks';
+import { ManageIcon } from 'ui/atoms/icons';
 
+import { useStyles } from './List.styles';
 export const List = () => {
   const { formatMessage } = useLocale();
+  const classes = useStyles();
+  const [showAllocateResults, setShowAllocateResults] = useState(true);
+  const tabs = [
+    {
+      key: 'ResultList',
+    },
+  ];
+  const [status, setStatus] = useState(tabs[0]);
+
+  const [] = useState([]);
 
   return (
     <Page withoutHeader>
-      <Card>
-        <CardContent>
-          <InfoSection emoji="🤔">
-            <Typography variant="h3">
-              {formatMessage({ id: 'project.details.allocate_results.empty_title' })}
-            </Typography>
-            <Typography variant="h3">
-              {formatMessage({ id: 'project.details.allocate_results.empty_description' })}
-            </Typography>
-          </InfoSection>
-        </CardContent>
+      <Card onClick={() => setShowAllocateResults(true)}>
+        {showAllocateResults && (
+          <>
+            <CardHeader
+              title={formatMessage({ id: 'project.details.allocate_results.title' })}
+              action={
+                <IconButton variant="roundedContained" onClick={() => {}} size="small">
+                  <ManageIcon />
+                </IconButton>
+              }
+            />
+            <CardContent className={classes.noPadding}>
+              <Box>
+                <Tabs
+                  className={classes.tabs}
+                  value={status.key}
+                  onChange={(event, value) => setStatus(value)}
+                  indicatorColor="primary"
+                  textColor="inherit"
+                >
+                  <Tab
+                    key={status.key}
+                    value={status.key}
+                    label={formatMessage({ id: `project.details.allocate_results.${status.key}` })}
+                  />
+                </Tabs>
+              </Box>
+            </CardContent>
+          </>
+        )}
+        {!showAllocateResults && (
+          <CardContent>
+            <InfoSection emoji="🤔">
+              <Typography variant="h3">
+                {formatMessage({ id: 'project.details.allocate_results.empty_title' })}
+              </Typography>
+              <Typography variant="h3">
+                {formatMessage({ id: 'project.details.allocate_results.empty_description' })}
+              </Typography>
+            </InfoSection>
+          </CardContent>
+        )}
       </Card>
     </Page>
   );
---
+++ src/app/projectDetails/sections/allocateResults/list/List.types.ts
@@ -0,0 +1,6 @@
+import { DateTime } from 'luxon';
+
+export type AllocateResultItem = {
+  assignee: string;
+  date: DateTime;
+};
