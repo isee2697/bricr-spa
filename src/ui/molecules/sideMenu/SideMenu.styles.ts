ba76088be6f096fc9a611210c1ff16443e903ca1
isee2697 <isee2697@outlook.com>
2020-11-05 08:37:22 +0000

    SideMenu.styles.ts edited online with Bitbucket

--- src/ui/molecules/sideMenu/SideMenu.styles.ts
+++ src/ui/molecules/sideMenu/SideMenu.styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(({ spacing }) => ({
   list: {
-    width: `100%`,
+    width: '100%',
     paddingLeft: spacing(3),
     overflow: 'hidden',
   },
