86d9544fcd24d8c79303cfcc3b8dff62d8b7885a
isee2697 <isee2697@outlook.com>
2020-11-05 18:41:02 +0100

    added styling ifx

--- src/ui/molecules/sidebarMenu/SidebarMenu.styles.ts
+++ src/ui/molecules/sidebarMenu/SidebarMenu.styles.ts
@@ -13,6 +13,7 @@ export const useStyles = ({ width, bannerColor }: { width: number | string; bann
       top: spacing(8),
       paddingTop: spacing(2),
       position: 'fixed',
+      zIndex: 100,
       overflowY: 'hidden',
       '& a': {
         textDecoration: 'none',
