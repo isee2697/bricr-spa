3d3b7d491a828f6e23c7e08e4b5c402c5b7a618f
bigcodeben <dempsterbill44@gmail.com>
2020-12-15 22:14:38 -0500

    UL-1664 Fix feedback

--- src/app/dashboard/dashboardAgenda/DashboardAgenda.styles.ts
+++ src/app/dashboard/dashboardAgenda/DashboardAgenda.styles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(theme => ({
   tabs: {
     '& .MuiTab-root': {
-      minWidth: 90,
+      minWidth: theme.spacing(11.25),
     },
   },
   scrollable: {
