4368d8293a56bf39170ae1579282c4d8308ffc81
iamwkev <iamwkev@gmail.com>
2020-12-28 07:05:41 -0800

    Fix Dashboard Agenda items

---
+++ src/app/dashboard/dashboardAgenda/DashboardAgendaItem.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { DateTime } from 'luxon';
+import { useHistory } from 'react-router-dom';
+
+import { Box, Typography } from 'ui/atoms';
+import { Appointment } from 'api/types';
+import { AppRoute } from 'routing/AppRoute.enum';
+import { useNylasAccountState } from 'hooks';
+
+import { useStyles } from './DashboardAgenda.styles';
+
+export const DashboardAgendaItem = ({ id, from, to, title }: Appointment) => {
+  const classes = useStyles();
+  const { push } = useHistory();
+
+  const { accounts } = useNylasAccountState();
+
+  return (
+    <Box
+      alignItems="center"
+      display="flex"
+      flexWrap="nowrap"
+      className={classes.task}
+      onClick={() => push(AppRoute.calendarAppointments.replace(':accountId', accounts[0].id))}
+    >
+      <Box className={classes.date}>
+        <Typography className={classes.startDate} variant="h5">
+          {from ? DateTime.fromISO(from).toLocaleString(DateTime.DATE_MED) : '-'}
+        </Typography>
+        <Typography className={classes.endDate} variant="h5">
+          {to ? DateTime.fromISO(to).toLocaleString(DateTime.DATE_MED) : '-'}
+        </Typography>
+      </Box>
+      <Box className={classes.title}>{title}</Box>
+    </Box>
+  );
+};
--- src/context/nylasContext/nylasAccountController/NylasAccountController.tsx
+++ src/context/nylasContext/nylasAccountController/NylasAccountController.tsx
@@ -16,32 +16,8 @@ export const NylasAccountController = ({ children }: NylasAccountControllerProps
   });
 
   useEffect(() => {
-    const getNylasAccounts = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_FILE_URL}/nylas-account-list?accountId=${user?.id}&isCalendarConnected=true`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: 'Bearer ' + accessToken,
-            },
-          },
-        );
-
-        if (response.ok) {
-          const accounts = await response.json();
-          dispatch(setNylasAccounts(accounts));
-        } else {
-          throw new Error('Error returned while getting nylas account list');
-        }
-      } catch (e) {
-        dispatch(setNylasAccounts([]));
-      }
-    };
-
-    if (user) {
-      getNylasAccounts();
+    if (!error && data?.listNylasAccount) {
+      dispatch(setNylasAccounts(data.listNylasAccount));
     }
   }, [dispatch, error, data, loading, user, accessToken]);
 
