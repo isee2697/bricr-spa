0d9f5be4d75e3587b6066bae0df83ecceca5abaa
iamwkev <iamwkev@gmail.com>
2020-12-28 08:17:13 -0800

    Fix relations container

--- src/app/crm/relations/RelationsContainer.tsx
+++ src/app/crm/relations/RelationsContainer.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
-// import { useCrmListQuery } from 'api/types';
+import { useCrmListQuery } from 'api/types';
 import { CRM as mockCrm } from 'api/mocks/crm';
-// import { CrmItem } from '../Crm.types';
+import { CrmItem } from '../Crm.types';
 
 import { RelationsContainerProps } from './Relations.types';
 import { Relations } from './Relations';
 
 export const RelationsContainer = (props: RelationsContainerProps) => {
-  // const { data } = useCrmListQuery();
+  const { data } = useCrmListQuery();
 
-  // const crms: CrmItem[] = (data?.crmList || []).map(crm => ({ ...mockCrm, ...crm }));
-  const crms = [mockCrm];
+  const crms: CrmItem[] = (data?.crmList || []).map(crm => ({ ...mockCrm, ...crm }));
 
   return <Relations {...props} crms={crms} />;
 };
