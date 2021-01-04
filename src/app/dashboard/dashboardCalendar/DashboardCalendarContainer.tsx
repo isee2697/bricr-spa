cfdbd60bc15fc946582fb52cba9edd1d3067a407
iamwkev <iamwkev@gmail.com>
2021-01-04 01:52:05 -0800

    Fix mock data

--- src/app/dashboard/dashboardCalendar/DashboardCalendarContainer.tsx
+++ src/app/dashboard/dashboardCalendar/DashboardCalendarContainer.tsx
@@ -26,13 +26,6 @@ export const DashboardCalendarContainer = () => {
     fetchPolicy: 'no-cache',
   });
 
-  // const laterToday = new Date();
-  // laterToday.setHours(laterToday.getHours() + 2);
-  // const tomorrow = new Date();
-  // tomorrow.setDate(tomorrow.getDate() + 1);
-  // const future = new Date();
-  // future.setDate(future.getDate() + 2);
-
   const data =
     agenda?.listCalendar?.map(item => ({
       isAllDay: item.allDay || false,
@@ -40,45 +33,6 @@ export const DashboardCalendarContainer = () => {
       endDate: new Date(item.to).toISOString(),
       title: item.title || '',
     })) || [];
-
-  // const data = [
-  //   {
-  //     isAllDay: false,
-  //     startDate: future.toISOString(),
-  //     endDate: future.toISOString(),
-  //     title: 'My Future appointment',
-  //   },
-  //   {
-  //     isAllDay: false,
-  //     startDate: new Date().toISOString(),
-  //     endDate: laterToday.toISOString(),
-  //     title: 'My Today appointment',
-  //   },
-  //   {
-  //     isAllDay: false,
-  //     startDate: new Date().toISOString(),
-  //     endDate: laterToday.toISOString(),
-  //     title: 'My Second Today appointment',
-  //   },
-  //   {
-  //     isAllDay: false,
-  //     startDate: new Date().toISOString(),
-  //     endDate: laterToday.toISOString(),
-  //     title: 'My Thirth Today appointment',
-  //   },
-  //   {
-  //     isAllDay: false,
-  //     startDate: tomorrow.toISOString(),
-  //     endDate: tomorrow.toISOString(),
-  //     title: 'My Tomorrow appointment',
-  //   },
-  //   {
-  //     isAllDay: false,
-  //     startDate: tomorrow.toISOString(),
-  //     endDate: tomorrow.toISOString(),
-  //     title: 'My Tomorrow appointment 2',
-  //   },
-  // ];
 
   return <DashboardCalendar onMoreClick={() => push(AppRoute.calendar)} data={data} />;
 };
