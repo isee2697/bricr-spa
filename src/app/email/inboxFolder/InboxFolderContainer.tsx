c88b0d84e8f236718cba7fe31ad0b282dcf85f9c
bigcodeben <dempsterbill44@gmail.com>
2021-01-07 04:28:23 -0500

    Fix emailing

--- src/app/email/inboxFolder/InboxFolderContainer.tsx
+++ src/app/email/inboxFolder/InboxFolderContainer.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { EmailListItem, useListEmailLazyQuery } from 'api/types';
+import { EmailFolderListItem, EmailListItem, useListEmailLazyQuery } from 'api/types';
 
 import { InboxFolder } from './InboxFolder';
 import { InboxFolderContainerProps } from './InboxFolder.types';
@@ -9,8 +9,9 @@ import { InboxFolderContainerProps } from './InboxFolder.types';
 export const InboxFolderContainer = (props: InboxFolderContainerProps) => {
   const { inboxId, folderId } = useParams<{ inboxId: string; folderId: string }>();
   const { folders } = props;
+  const [currentFolder, setCurrentFolder] = useState<EmailFolderListItem>();
 
-  const [listEmails, { data, loading: loadingEmails }] = useListEmailLazyQuery({ fetchPolicy: 'no-cache' });
+  const [listEmails, { data }] = useListEmailLazyQuery({ fetchPolicy: 'no-cache' });
 
   useEffect(() => {
     const getEmails = async () => {
@@ -25,14 +26,13 @@ export const InboxFolderContainer = (props: InboxFolderContainerProps) => {
     getEmails();
   }, [folderId, inboxId, listEmails]);
 
-  const currentFolder = folders.find(folder => folder.folder.nylasFolderId === folderId);
+  useEffect(() => {
+    const selectedFolder = folders.find(folder => folder.folder.nylasFolderId === folderId);
 
-  return (
-    <InboxFolder
-      {...props}
-      emails={(data?.listEmail || []) as EmailListItem[]}
-      loading={loadingEmails || !currentFolder}
-      currentFolder={currentFolder}
-    />
-  );
+    if (selectedFolder) {
+      setCurrentFolder(selectedFolder);
+    }
+  }, [folderId, folders, folders.length]);
+
+  return <InboxFolder {...props} emails={(data?.listEmail || []) as EmailListItem[]} currentFolder={currentFolder} />;
 };
