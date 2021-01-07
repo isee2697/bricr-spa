e96b58895acbcdad77004ed6d60eacf2b4ca29db
iamwkev <iamwkev@gmail.com>
2021-01-07 04:32:20 -0800

    Link email to Relations

--- src/app/email/inboxFolder/InboxFolder.tsx
+++ src/app/email/inboxFolder/InboxFolder.tsx
@@ -16,7 +16,6 @@ import {
   Select,
   Typography,
 } from 'ui/atoms';
-import { EmailSidebarMenuContainer } from '../emailSidebarMenu/EmailSidebarMenuContainer';
 import { EmailHeader } from '../emailHeader/EmailHeader';
 import { AddIcon, ManageIcon, SearchIcon, SettingsIcon } from 'ui/atoms/icons';
 import { Page } from 'ui/templates';
@@ -65,104 +64,99 @@ export const InboxFolder = ({
 
   return (
     <>
-      <EmailSidebarMenuContainer
-        onHide={onSidebarClose}
-        isVisible={isSidebarVisible}
-        accounts={accounts}
-        folders={folders}
-      />
-      <Grid item xs={12} md={9} lg={10}>
-        {loading && <Loader />}
-        {!loading && (
-          <Grid container className={classes.content}>
-            <EmailHeader
-              onSidebarOpen={onSidebarOpen}
-              isSidebarVisible={isSidebarVisible}
-              actions={
-                <Box display="flex" alignItems="center">
-                  <Box mr={2}>
-                    <IconButton
-                      variant="rounded"
-                      size="small"
-                      onClick={() => push(`${joinUrlParams(AppRoute.email, urlParams)}/settings`)}
-                    >
-                      <SettingsIcon />
-                    </IconButton>
-                  </Box>
-                  <Button
-                    color="primary"
-                    variant="contained"
-                    startIcon={<AddIcon color="inherit" />}
-                    onClick={() => {
-                      open('compose-new-email');
-                    }}
+      {loading && <Loader />}
+      {!loading && (
+        <Grid container className={classes.content}>
+          <EmailHeader
+            onSidebarOpen={onSidebarOpen}
+            isSidebarVisible={isSidebarVisible}
+            actions={
+              <Box display="flex" alignItems="center">
+                <Box mr={2}>
+                  <IconButton
+                    variant="rounded"
+                    size="small"
+                    onClick={() => push(`${joinUrlParams(AppRoute.email, urlParams)}/settings`)}
                   >
-                    {formatMessage({ id: 'email.inbox.new_email' })}
-                  </Button>
+                    <SettingsIcon />
+                  </IconButton>
                 </Box>
-              }
-            />
-            <Page withoutHeader>
-              {emails.length > 0 && (
-                <Card>
-                  <CardHeader
-                    title={currentFolder?.folder.displayName}
-                    action={
-                      <Box display="flex" alignItems="center">
-                        <IconButton size="small" variant="roundedContained">
-                          <SearchIcon />
-                        </IconButton>
-                        <Box mr={1.5} />
-                        <IconButton size="small" variant="roundedContained">
-                          <ManageIcon />
-                        </IconButton>
-                      </Box>
-                    }
-                  />
-                  <CardContent>
-                    <Box display="flex" justifyContent="space-between" alignItems="center">
-                      <Box ml={0.5} display="flex" alignItems="center">
-                        <Checkbox
-                          color="primary"
-                          checked={emails.length === checkedEmails.length}
-                          onClick={handleCheckAllEmails}
-                        />
-                        <Typography variant="h5" color="textSecondary">
-                          {formatMessage({ id: 'common.select_all' })}
-                        </Typography>
-                      </Box>
-                      <Select variant="outlined" value={sortOptions[0]} className={classes.sort}>
-                        {sortOptions.map(option => (
-                          <MenuItem key={option} value={option}>
-                            {formatMessage({ id: `common.sort_options.${option}` })}
-                          </MenuItem>
-                        ))}
-                      </Select>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  startIcon={<AddIcon color="inherit" />}
+                  onClick={() => {
+                    open('compose-new-email');
+                  }}
+                >
+                  {formatMessage({ id: 'email.inbox.new_email' })}
+                </Button>
+              </Box>
+            }
+          />
+          <Page withoutHeader>
+            {emails.length > 0 && (
+              <Card>
+                <CardHeader
+                  title={currentFolder?.folder.displayName}
+                  action={
+                    <Box display="flex" alignItems="center">
+                      <IconButton size="small" variant="roundedContained">
+                        <SearchIcon />
+                      </IconButton>
+                      <Box mr={1.5} />
+                      <IconButton size="small" variant="roundedContained">
+                        <ManageIcon />
+                      </IconButton>
                     </Box>
-                    <EmailTable
-                      emails={emails}
-                      checkedItems={checkedEmails}
-                      onCheckItem={handleCheckEmail}
-                      onCheckAllItems={() => handleCheckAllEmails()}
-                    />
-                  </CardContent>
-                </Card>
-              )}
-              {emails.length === 0 && (
-                <Card>
-                  <CardHeader title={currentFolder?.folder.displayName} />
-                  <CardContent>
-                    <InfoSection emoji="🎉">
-                      <Typography variant="h3">{formatMessage({ id: 'email.inbox.empty_title' })}</Typography>
-                      <Typography variant="h3">{formatMessage({ id: 'email.inbox.empty_description' })}</Typography>
-                    </InfoSection>
-                  </CardContent>
-                </Card>
-              )}
-            </Page>
-          </Grid>
-        )}
-      </Grid>
+                  }
+                />
+                <CardContent>
+                  <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Box ml={0.5} display="flex" alignItems="center">
+                      <Checkbox
+                        color="primary"
+                        checked={emails.length === checkedEmails.length}
+                        onClick={handleCheckAllEmails}
+                      />
+                      <Typography variant="h5" color="textSecondary">
+                        {formatMessage({ id: 'common.select_all' })}
+                      </Typography>
+                    </Box>
+                    <Select variant="outlined" value={sortOptions[0]} className={classes.sort}>
+                      {sortOptions.map(option => (
+                        <MenuItem key={option} value={option}>
+                          {formatMessage({
+                            id: `common.sort_options.${option}`,
+                          })}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </Box>
+                  <EmailTable
+                    emails={emails}
+                    checkedItems={checkedEmails}
+                    onCheckItem={handleCheckEmail}
+                    onCheckAllItems={() => handleCheckAllEmails()}
+                  />
+                </CardContent>
+              </Card>
+            )}
+            {emails.length === 0 && (
+              <Card>
+                <CardHeader title={currentFolder?.folder.displayName} />
+                <CardContent>
+                  <InfoSection emoji="🎉">
+                    <Typography variant="h3">{formatMessage({ id: 'email.inbox.empty_title' })}</Typography>
+                    <Typography variant="h3">{formatMessage({ id: 'email.inbox.empty_description' })}</Typography>
+                  </InfoSection>
+                </CardContent>
+              </Card>
+            )}
+          </Page>
+        </Grid>
+      )}
+      {/* </Grid> */}
     </>
   );
 };
---
+++ src/app/email/linkCrmRelationModal/LinkCrmRelationModalContainer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { CreateCrmInput, CrmType, useCreateCrmMutation } from 'api/types';
+import { AddCrmRelationModal } from 'app/shared/addCrmRelationModal/AddCrmRelationModal';
+
+import { LinkCrmRelationModalContainerProps } from './LinkCrmRelationModalContainer.types';
+
+export const LinkCrmRelationModalContainer = ({ isOpened, onSubmit, onClose }: LinkCrmRelationModalContainerProps) => {
+  const [createCrm] = useCreateCrmMutation();
+
+  const createNewRelation = async (input: CreateCrmInput) => {
+    const { data, errors } = await createCrm({
+      variables: {
+        input: {
+          ...input,
+          type: CrmType.Relation,
+        },
+      },
+    });
+
+    if (errors) {
+      throw new Error();
+    }
+
+    if (data) {
+      onSubmit(data.createCrm);
+    }
+  };
+
+  return (
+    <AddCrmRelationModal
+      isOpened={isOpened}
+      onClose={onClose}
+      onCreateNewRelation={createNewRelation}
+      onRequestBricrData={() => {
+        onClose();
+      }}
+    />
+  );
+};
