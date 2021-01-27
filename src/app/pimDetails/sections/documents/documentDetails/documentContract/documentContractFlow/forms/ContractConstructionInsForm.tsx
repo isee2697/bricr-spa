ae464198202aa3f9a744ea95d5338b1319097b83
bigcodeben <dempsterbill44@gmail.com>
2021-01-27 04:43:42 -0800

    PIM document pages update

--- src/app/pimDetails/sections/documents/documentDetails/documentContract/documentContractFlow/forms/ContractConstructionInsForm.tsx
+++ src/app/pimDetails/sections/documents/documentDetails/documentContract/documentContractFlow/forms/ContractConstructionInsForm.tsx
@@ -1,55 +1,112 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 
 import { useLocale } from 'hooks';
 import { AutosaveForm, FormSection } from 'ui/organisms';
 import { DatePickerField, GenericField } from 'form/fields';
-import { Box, Checkbox, FormControlLabel, Grid, Typography } from 'ui/atoms';
+import { Box, Checkbox, FormControlLabel, Grid, Menu, MenuItem, Typography } from 'ui/atoms';
+import { DeleteIcon, HistoryIcon } from 'ui/atoms/icons';
+import { useStyles } from '../DocumentContractFlow.styles';
+
+type SubMenuItemType = {
+  title: string;
+  onClick?: VoidFunction;
+  icon?: ReactElement;
+};
+
+const SubMenuItem = ({ title, onClick, icon }: SubMenuItemType) => {
+  const classes = useStyles();
+
+  return (
+    <MenuItem
+      className={classes.menuItem}
+      onClick={(event: React.MouseEvent) => {
+        event.stopPropagation();
+        onClick?.();
+      }}
+    >
+      {icon ?? <HistoryIcon classes={{ root: classes.menuIcon }} />}
+      <Box ml={2}>
+        <Typography variant="subtitle1">{title}</Typography>
+      </Box>
+    </MenuItem>
+  );
+};
 
 export function ContractConstructionInsForm() {
   const { formatMessage } = useLocale();
+  const [menuEl, setMenuEl] = useState<HTMLElement | null>(null);
+
+  const onMenuClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.stopPropagation();
+    setMenuEl(menuEl ? null : event.currentTarget);
+  };
+
+  const onMenuClose = () => {
+    setMenuEl(null);
+  };
 
   return (
-    <FormSection
-      title={formatMessage({
-        id: 'pim_details.documents.construction_inspection',
-      })}
-      isExpandable
-      isInitExpanded
-    >
-      {editing => (
-        <AutosaveForm onSave={() => Promise.resolve(undefined)}>
-          <FormControlLabel
-            control={<Checkbox color="primary" />}
-            label={
-              <Typography variant="h5">
-                {formatMessage({ id: 'pim_details.documents.check_if_construction_inspection' })}
-              </Typography>
-            }
-          />
-          <Box mt={1} />
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm={4}>
-              <DatePickerField
-                label={formatMessage({
-                  id: 'pim_details.documents.construction_inspection_date',
-                })}
-                name="constructionInsDate"
-                disabled={!editing}
-                placeholder={formatMessage({ id: 'common.date_picker' })}
-              />
+    <>
+      <FormSection
+        title={formatMessage({
+          id: 'pim_details.documents.construction_inspection',
+        })}
+        isExpandable
+        isInitExpanded
+        onOptionsClick={onMenuClick}
+      >
+        {editing => (
+          <AutosaveForm onSave={() => Promise.resolve(undefined)}>
+            <FormControlLabel
+              control={<Checkbox color="primary" />}
+              label={
+                <Typography variant="h5">
+                  {formatMessage({ id: 'pim_details.documents.check_if_construction_inspection' })}
+                </Typography>
+              }
+            />
+            <Box mt={1} />
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={4}>
+                <DatePickerField
+                  label={formatMessage({
+                    id: 'pim_details.documents.construction_inspection_date',
+                  })}
+                  name="constructionInsDate"
+                  disabled={!editing}
+                  placeholder={formatMessage({ id: 'common.date_picker' })}
+                />
+              </Grid>
+              <Grid item xs={12} sm={8}>
+                <GenericField
+                  disabled={!editing}
+                  name="constructionInsBy"
+                  label="pim_details.documents.construction_inspection_by"
+                  placeholder="pim_details.documents.select_company_from_crm"
+                  size="medium"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={8}>
-              <GenericField
-                disabled={!editing}
-                name="constructionInsBy"
-                label="pim_details.documents.construction_inspection_by"
-                placeholder="pim_details.documents.select_company_from_crm"
-                size="medium"
-              />
-            </Grid>
-          </Grid>
-        </AutosaveForm>
-      )}
-    </FormSection>
+          </AutosaveForm>
+        )}
+      </FormSection>
+      <Menu
+        id="contract-construction-ins-form-menu"
+        open={Boolean(menuEl)}
+        onClose={onMenuClose}
+        anchorEl={menuEl}
+        placement="bottom-end"
+      >
+        <SubMenuItem
+          title={formatMessage({
+            id: 'common.delete',
+          })}
+          onClick={() => {
+            onMenuClose();
+          }}
+          icon={<DeleteIcon color="secondary" />}
+        />
+      </Menu>
+    </>
   );
 }
--- src/app/pimDetails/sections/documents/documentDetails/documentContract/documentContractFlow/forms/ContractSigningForm.tsx
+++ src/app/pimDetails/sections/documents/documentDetails/documentContract/documentContractFlow/forms/ContractSigningForm.tsx
@@ -1,55 +1,112 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 
 import { useLocale } from 'hooks';
 import { AutosaveForm, FormSection } from 'ui/organisms';
 import { DatePickerField, GenericField } from 'form/fields';
-import { Box, Checkbox, FormControlLabel, Grid, Typography } from 'ui/atoms';
+import { Box, Checkbox, FormControlLabel, Grid, Typography, Menu, MenuItem } from 'ui/atoms';
+import { useStyles } from '../DocumentContractFlow.styles';
+import { HistoryIcon, DeleteIcon } from 'ui/atoms/icons';
+
+type SubMenuItemType = {
+  title: string;
+  onClick?: VoidFunction;
+  icon?: ReactElement;
+};
+
+const SubMenuItem = ({ title, onClick, icon }: SubMenuItemType) => {
+  const classes = useStyles();
+
+  return (
+    <MenuItem
+      className={classes.menuItem}
+      onClick={(event: React.MouseEvent) => {
+        event.stopPropagation();
+        onClick?.();
+      }}
+    >
+      {icon ?? <HistoryIcon classes={{ root: classes.menuIcon }} />}
+      <Box ml={2}>
+        <Typography variant="subtitle1">{title}</Typography>
+      </Box>
+    </MenuItem>
+  );
+};
 
 export function ContractSigningForm() {
   const { formatMessage } = useLocale();
+  const [menuEl, setMenuEl] = useState<HTMLElement | null>(null);
+
+  const onMenuClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.stopPropagation();
+    setMenuEl(menuEl ? null : event.currentTarget);
+  };
+
+  const onMenuClose = () => {
+    setMenuEl(null);
+  };
 
   return (
-    <FormSection
-      title={formatMessage({
-        id: 'pim_details.documents.signing',
-      })}
-      isExpandable
-      isInitExpanded
-    >
-      {editing => (
-        <AutosaveForm onSave={() => Promise.resolve(undefined)}>
-          <FormControlLabel
-            control={<Checkbox color="primary" />}
-            label={
-              <Typography variant="h5">
-                {formatMessage({ id: 'pim_details.documents.check_if_signing_digital' })}
-              </Typography>
-            }
-          />
+    <>
+      <FormSection
+        title={formatMessage({
+          id: 'pim_details.documents.signing',
+        })}
+        isExpandable
+        isInitExpanded
+        onOptionsClick={onMenuClick}
+      >
+        {editing => (
+          <AutosaveForm onSave={() => Promise.resolve(undefined)}>
+            <FormControlLabel
+              control={<Checkbox color="primary" />}
+              label={
+                <Typography variant="h5">
+                  {formatMessage({ id: 'pim_details.documents.check_if_signing_digital' })}
+                </Typography>
+              }
+            />
 
-          <Box mt={1} />
-          <GenericField
-            disabled={!editing}
-            name="placePurchaseSigned"
-            label="pim_details.documents.place_purchase_signed"
-            size="medium"
-          />
+            <Box mt={1} />
+            <GenericField
+              disabled={!editing}
+              name="placePurchaseSigned"
+              label="pim_details.documents.place_purchase_signed"
+              size="medium"
+            />
 
-          <Box mt={1} />
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm={4}>
-              <DatePickerField
-                label={formatMessage({
-                  id: 'pim_details.documents.date_purchase_signed',
-                })}
-                name="datePurchaseSigned"
-                disabled={!editing}
-                placeholder={formatMessage({ id: 'common.date_picker' })}
-              />
+            <Box mt={1} />
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={4}>
+                <DatePickerField
+                  label={formatMessage({
+                    id: 'pim_details.documents.date_purchase_signed',
+                  })}
+                  name="datePurchaseSigned"
+                  disabled={!editing}
+                  placeholder={formatMessage({ id: 'common.date_picker' })}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </AutosaveForm>
-      )}
-    </FormSection>
+          </AutosaveForm>
+        )}
+      </FormSection>
+      <Menu
+        id="contract-form-menu"
+        open={Boolean(menuEl)}
+        onClose={onMenuClose}
+        anchorEl={menuEl}
+        placement="bottom-end"
+      >
+        <SubMenuItem
+          title={formatMessage({
+            id: 'common.delete',
+          })}
+          onClick={() => {
+            onMenuClose();
+          }}
+          icon={<DeleteIcon color="secondary" />}
+        />
+      </Menu>
+    </>
   );
 }
