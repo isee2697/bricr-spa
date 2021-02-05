aaf39e64dc66840698ec619ff703b226771db7c6
isee2697 <isee2697@outlook.com>
2021-02-05 12:10:23 +0100

    fixed readme added and color passing

--- src/ui/molecules/checkboxLoading/CheckboxLoading.tsx
+++ src/ui/molecules/checkboxLoading/CheckboxLoading.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Box, Checkbox, CircularProgress } from 'ui/atoms';
 import { CheckboxProps } from 'ui/atoms/checkbox/Checkbox.types';
 
-export const CheckboxLoading = ({ isLoading, ...props }: CheckboxProps & { isLoading: boolean }) => {
+export const CheckboxLoading = ({ isLoading, color = 'primary', ...props }: CheckboxProps & { isLoading: boolean }) => {
+  const passColor = color === 'secondary' || color === 'primary' ? color : undefined;
+
   return isLoading ? (
     <Box ml={1.5} mr={1.5}>
-      <CircularProgress size={18} />
+      <CircularProgress color={passColor} size={18} />
     </Box>
   ) : (
-    <Checkbox color="primary" {...props} />
+    <Checkbox color={passColor} {...props} />
   );
 };
