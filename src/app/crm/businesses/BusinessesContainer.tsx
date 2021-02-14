df650d1b9b403c15a6fb8356ff2ab8d6c0310da2
bigcodeben <dempsterbill44@gmail.com>
2021-02-14 17:42:41 -0500

    UL-2693 CRM relations filters

--- src/app/crm/businesses/BusinessesContainer.tsx
+++ src/app/crm/businesses/BusinessesContainer.tsx
@@ -3,18 +3,18 @@ import React, { useState } from 'react';
 import { CrmItem } from '../Crm.types';
 import { CRM as mockCrm } from 'api/mocks/crm';
 import { CRM_BUSINESSES } from 'api/mocks/crm-business';
-import { ListPimsFilters, PropertyType } from 'api/types';
+import { CrmType, ListCrmFilters } from 'api/types';
 
 import { BusinessesContainerProps } from './Businesses.types';
 import { Businesses } from './Businesses';
 
 export const BusinessesContainer = (props: BusinessesContainerProps) => {
   const crms: CrmItem[] = CRM_BUSINESSES.map(crm => ({ ...mockCrm, ...crm, status: props.status }));
-  const [activeFilters, setActiveFilters] = useState<ListPimsFilters>({
-    propertyTypes: [PropertyType.Apartment, PropertyType.House],
+  const [activeFilters, setActiveFilters] = useState<ListCrmFilters>({
+    type: CrmType.Business,
   });
 
-  const handleFilterChange = (filters: ListPimsFilters) => {
+  const handleFilterChange = (filters: ListCrmFilters) => {
     setActiveFilters(filters);
   };
 
