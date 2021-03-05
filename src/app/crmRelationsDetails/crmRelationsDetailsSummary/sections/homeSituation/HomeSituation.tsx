0f59739b6328ea7eb8a8502789fd1d81289c9534
bigcodeben <dempsterbill44@gmail.com>
2021-03-04 22:01:09 -0500

    UL-2726 Card with children

--- src/app/crmRelationsDetails/crmRelationsDetailsSummary/sections/homeSituation/HomeSituation.tsx
+++ src/app/crmRelationsDetails/crmRelationsDetailsSummary/sections/homeSituation/HomeSituation.tsx
@@ -4,12 +4,21 @@ import { useLocale } from 'hooks';
 import { Box, Typography } from 'ui/atoms';
 import { FormSection } from 'ui/organisms';
 import { useStyles } from '../../CrmRelationsDetailsSummary.styles';
+import { CrmHomeSituation } from 'api/types';
 
 import { HomeSituationProps } from './HomeSituation.types';
 
 export const HomeSituation = ({ crm }: HomeSituationProps) => {
   const { formatMessage } = useLocale();
   const classes = useStyles();
+
+  const mockData: CrmHomeSituation = {
+    id: '0001',
+    currentHomeSituation: 'Livein',
+    currentHomeSalesValue: 24500000,
+    currentHomeMortgage: 19900000,
+    reasonToMove: ['Change of income'],
+  };
 
   return (
     <FormSection
@@ -22,7 +31,10 @@ export const HomeSituation = ({ crm }: HomeSituationProps) => {
           {formatMessage({ id: 'common.current_home' })}
         </Typography>
         <Typography variant="h4" className={classes.fontWeightMedium}>
-          Live-in
+          {formatMessage(
+            { id: 'crm.details.summary.home_situation.current_home_situation' },
+            { situation: mockData.currentHomeSituation },
+          )}
         </Typography>
       </Box>
       <Box mb={2}>
@@ -30,7 +42,10 @@ export const HomeSituation = ({ crm }: HomeSituationProps) => {
           {formatMessage({ id: 'common.current_home' })}
         </Typography>
         <Typography variant="h4" className={classes.fontWeightMedium}>
-          Yes, for sale, estimated € 245.000,00
+          {formatMessage(
+            { id: 'crm.details.summary.home_situation.current_home.value' },
+            { price: mockData.currentHomeSalesValue },
+          )}
         </Typography>
       </Box>
       <Box mb={2}>
@@ -38,16 +53,18 @@ export const HomeSituation = ({ crm }: HomeSituationProps) => {
           {formatMessage({ id: 'common.mortgage' })}
         </Typography>
         <Typography variant="h4" className={classes.fontWeightMedium}>
-          € 199.000,00
+          € {mockData.currentHomeMortgage}
         </Typography>
       </Box>
       <Box>
         <Typography variant="h5" color="textSecondary" className={classes.fontWeightMedium}>
           {formatMessage({ id: 'common.reason_to_move' })}
         </Typography>
-        <Typography variant="h4" className={classes.fontWeightMedium}>
-          Change of income
-        </Typography>
+        {mockData.reasonToMove?.map(reason => (
+          <Typography variant="h4" className={classes.fontWeightMedium}>
+            {reason}
+          </Typography>
+        ))}
       </Box>
     </FormSection>
   );
--- src/app/crmRelationsDetails/crmRelationsDetailsSummary/sections/personalInformation/PersonalInformation.tsx
+++ src/app/crmRelationsDetails/crmRelationsDetailsSummary/sections/personalInformation/PersonalInformation.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 
-import { useLocale } from 'hooks';
-import { Box, Card, CardContent, CardHeader } from 'ui/atoms';
+import { Box } from 'ui/atoms';
+import { CardWithBody } from 'ui/templates';
 
 import { PersonalInformationProps } from './PersonalInformation.types';
 import { PersonalInformationItem } from './personalInformationItem/PersonalInformationItem';
 
 export const PersonalInformation = ({ crm }: PersonalInformationProps) => {
-  const { formatMessage } = useLocale();
   const { partner } = crm;
 
   return (
-    <Card>
-      <CardHeader title={formatMessage({ id: 'crm.details.summary.personal_information' })} />
-      <CardContent>
-        <PersonalInformationItem {...crm} />
-        {partner && (
-          <Box mt={2}>
-            <PersonalInformationItem {...partner} />
-          </Box>
-        )}
-      </CardContent>
-    </Card>
+    <CardWithBody titleId={'crm.details.summary.personal_information'}>
+      <PersonalInformationItem {...crm} />
+      {partner && (
+        <Box mt={2}>
+          <PersonalInformationItem {...partner} />
+        </Box>
+      )}
+    </CardWithBody>
   );
 };
