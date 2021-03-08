e7632cd2a5c38ba69bf4a8b35157f6109f378456
isee2697 <isee2697@outlook.com>
2021-03-07 20:24:21 -0500

    UL-3096 Imageholder unit test

---
+++ src/ui/atoms/imageHolder/ImageHolder.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { render } from 'tests';
+import { palette } from 'theme/palette';
+
+import { ImageHolder } from './ImageHolder';
+
+describe('ImageHolder', () => {
+  it('render correctly', () => {
+    const image = 'https://source.unsplash.com/featured/?building';
+
+    const { container } = render(<ImageHolder src={image} />);
+
+    expect(container.getElementsByClassName('MuiGrid-root').length).toEqual(1);
+  });
+
+  it('render with border', () => {
+    const image = 'https://source.unsplash.com/featured/?building';
+
+    const { container } = render(<ImageHolder src={image} withBorder />);
+
+    const elements = container.getElementsByClassName('MuiGrid-root');
+
+    expect(elements[0]).toHaveStyle(`border: 1px ${palette.gray.main} solid`);
+  });
+});
