24a83924491be423451b66075bb4c970ff3198e9
pgeoff <pgeoff057@gmail.com>
2021-03-07 19:35:53 -0500

    UL-3025 Fix test

--- src/app/shared/addCrmRelationModal/AddCrmRelationModal.tsx
+++ src/app/shared/addCrmRelationModal/AddCrmRelationModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Form, useForm } from 'react-final-form';
+import { Form } from 'react-final-form';
 
 import { Modal } from 'ui/molecules';
 import { useLocale } from 'hooks';
