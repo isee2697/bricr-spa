2a89d2e286d730baccdc543fd64f94e4c70cd796
isee2697 <isee2697@outlook.com>
2021-03-10 10:39:56 -0500

    UL-3013 Pagination test

--- src/ui/atoms/pagination/Pagination.test.tsx
+++ src/ui/atoms/pagination/Pagination.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from 'tests';
+import { fireEvent, render } from 'tests';
 
 import { Pagination } from './Pagination';
 
@@ -9,16 +9,23 @@ describe('Pagination', () => {
     const onPerPageChange = jest.fn();
 
     const { getByText } = render(
-      <Pagination
-        count={8}
-        currentPerPage={10}
-        perPageOptions={[10, 20, 'All']}
-        onPerPageChange={value => onPerPageChange()}
-      />,
+      <Pagination count={8} currentPerPage={10} perPageOptions={[10, 20, 'All']} onPerPageChange={onPerPageChange} />,
     );
 
     expect(getByText('1')).toBeTruthy();
     expect(getByText('4')).toBeTruthy();
     expect(getByText('8')).toBeTruthy();
+
+    const prevButton = getByText('prev');
+    const nextButton = getByText('next');
+
+    expect(prevButton).toBeInTheDocument();
+    expect(nextButton).toBeInTheDocument();
+
+    fireEvent.click(nextButton);
+    expect(getByText('2')).toHaveAttribute('aria-current', 'true');
+
+    fireEvent.click(prevButton);
+    expect(getByText('1')).toHaveAttribute('aria-current', 'true');
   });
 });
