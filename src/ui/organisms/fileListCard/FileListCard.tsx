a04d8cac01c7e73f2d0c9f517519385d47801e50
isee2697 <isee2697@outlook.com>
2021-03-10 09:48:39 +0100

    test

---
+++ src/ui/organisms/fileListCard/FileListCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+
+import { FileListCardProps } from './FileListCard.types';
+
+export const FileListCard = ({ disableUpload }: FileListCardProps) => {
+  return <Card>test</Card>;
+};
---
+++ src/ui/organisms/fileListCard/FileListCard.types.ts
@@ -0,0 +1,3 @@
+export type FileListCardProps = {
+  disableUpload?: boolean;
+};
