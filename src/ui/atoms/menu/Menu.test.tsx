a005e73646e2814a5975a305d6d0bb6c1dbf2122
isee2697 <isee2697@outlook.com>
2021-03-10 09:03:54 -0500

    UL-3101 Menu item click test

--- src/ui/atoms/menu/Menu.test.tsx
+++ src/ui/atoms/menu/Menu.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from 'tests';
+import { fireEvent, render } from 'tests';
 
 import { Menu } from './Menu';
 
@@ -17,5 +17,26 @@ describe('Menu', () => {
     );
 
     expect(getByText('bottom-end')).toBeTruthy();
+  });
+
+  it('on click function', () => {
+    const onClose = jest.fn();
+    const onClick = jest.fn();
+
+    const { getByText } = render(
+      <Menu open onClose={onClose} id="test-menu">
+        <MenuItem value="bottom-end" onClick={onClick}>
+          bottom-end
+        </MenuItem>
+      </Menu>,
+    );
+
+    const menuItem = getByText('bottom-end');
+
+    expect(menuItem).toBeInTheDocument();
+
+    fireEvent.click(menuItem);
+
+    expect(onClick).toBeCalled();
   });
 });
