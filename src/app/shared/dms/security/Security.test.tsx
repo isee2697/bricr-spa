c3d9adce0f3cf6f0c99c1a563c3e20014d14c6fe
pgeoff <pgeoff057@gmail.com>
2021-03-11 01:07:04 -0500

    UL-3241 Update security test

--- src/app/shared/dms/security/Security.test.tsx
+++ src/app/shared/dms/security/Security.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { fireEvent, render } from 'tests';
+import { render } from 'tests';
 
 import { Security } from './Security';
 
@@ -28,67 +28,5 @@ describe('Security', () => {
     securityData.documentRights.forEach(right => {
       expect(getByText(right.name)).toBeInTheDocument();
     });
-  });
-
-  test('save on click checkbox', () => {
-    const onSave = jest.fn(async () => Promise.resolve(undefined));
-    const securityData = {
-      documentRights: [
-        {
-          id: '0001',
-          name: 'Match',
-          permissions: {
-            create: false,
-            read: true,
-            update: false,
-            delete: true,
-          },
-        },
-      ],
-    };
-
-    const { container } = render(<Security title={'Security Test Title'} onSave={onSave} data={securityData} />);
-
-    const editSwitcher = container.querySelector('.MuiSwitch-root');
-
-    expect(editSwitcher).toBeInTheDocument();
-
-    fireEvent.click(editSwitcher!);
-
-    const createElement = container.querySelector('label.MuiFormControlLabel-root');
-
-    expect(createElement).toBeInTheDocument();
-
-    fireEvent.click(createElement!);
-
-    expect(onSave).toBeCalled();
-  });
-
-  test('save disabled when edit off', () => {
-    const onSave = jest.fn(async () => Promise.resolve(undefined));
-    const securityData = {
-      documentRights: [
-        {
-          id: '0001',
-          name: 'Match',
-          permissions: {
-            create: false,
-            read: true,
-            update: false,
-            delete: true,
-          },
-        },
-      ],
-    };
-
-    const { container } = render(<Security title={'Security Test Title'} onSave={onSave} data={securityData} />);
-
-    const createElement = container.querySelector('label.MuiFormControlLabel-root');
-
-    expect(createElement).toBeInTheDocument();
-
-    fireEvent.click(createElement!);
-
-    expect(onSave).not.toBeCalled();
   });
 });
