6cf93559c34eb483a8469fb249dbb2b3bf703160
isee2697 <isee2697@outlook.com>
2021-03-11 04:19:53 -0500

    UL-3119 VisitedPage test for sub category, name

--- src/ui/atoms/visitedPage/VisitedPage.test.tsx
+++ src/ui/atoms/visitedPage/VisitedPage.test.tsx
@@ -9,5 +9,7 @@ describe('VisitedPage', () => {
     const { getByText } = render(<VisitedPage category="PIM" subCategory="Sale" name="Weerschijnvlinder 8: Prijzen" />);
 
     expect(getByText('Weerschijnvlinder 8: Prijzen')).toBeTruthy();
+    expect(getByText(/common.pages.sub_catgeory.Sale/)).toBeTruthy();
+    expect(getByText('Weerschijnvlinder 8: Prijzen')).toBeTruthy();
   });
 });
