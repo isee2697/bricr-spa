32b9659dce9694e385c83dca43f71aad9f9219bc
bigcodeben <dempsterbill44@gmail.com>
2021-03-14 14:55:21 -0400

    UL-3171 Recover for other types

--- src/app/dms/dmsPims/dmsFolders/dmsListView/DmsListViewContainer.tsx
+++ src/app/dms/dmsPims/dmsFolders/dmsListView/DmsListViewContainer.tsx
@@ -3,8 +3,13 @@ import React, { useState } from 'react';
 import { ListPimsFilters, PropertyType } from 'api/types';
 import { EmailList } from 'app/shared/emailList/EmailList';
 import { EMAILS } from 'api/mocks/email';
+import { Box } from 'ui/atoms';
 
 import { DmsListViewContainerProps } from './DmsListViewContainer.types';
+import { DmsViewTabs } from './dmsViewTabs/DmsViewTabs';
+import { DmsDetailView } from './dmsDetailView/DmsDetailView';
+import { DmsTableView } from './dmsTableView/DmsTableView';
+import { DmsViewType } from './dmsViewTabs/DmsViewTabs.types';
 
 export const DmsListViewContainer = ({
   id,
@@ -19,6 +24,7 @@ export const DmsListViewContainer = ({
   const [activeFilters, setActiveFilters] = useState<ListPimsFilters>({
     propertyTypes: [PropertyType.Apartment, PropertyType.House],
   });
+  const [view, setView] = useState<DmsViewType>('detail');
 
   return (
     <>
@@ -35,6 +41,22 @@ export const DmsListViewContainer = ({
           onAdd={() => {}}
           count={EMAILS.length}
         />
+      )}
+      {category !== 'crm' && (
+        <Box width="100%" display="flex" flexDirection="column">
+          <Box ml="auto" mr={1.5}>
+            <DmsViewTabs
+              status={view}
+              onStatusChange={setView}
+              activeFilters={activeFilters}
+              onFilter={setActiveFilters}
+              onAdd={() => {}}
+            />
+          </Box>
+          <Box mt={7}>
+            {view === 'detail' ? <DmsDetailView data={data || []} /> : <DmsTableView data={data || []} />}
+          </Box>
+        </Box>
       )}
     </>
   );
