c0bde7b7a2571f66eabdd532ac39636190929466
bigcodeben <dempsterbill44@gmail.com>
2021-03-16 14:08:23 -0400

    UL-3129 Dashboard header test

--- src/ui/molecules/dashboardHeader/DashboardHeader.test.tsx
+++ src/ui/molecules/dashboardHeader/DashboardHeader.test.tsx
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import React from 'react';
 
 import { render } from 'tests';
@@ -10,6 +11,24 @@ describe('DashboardHeader', () => {
     const { getByText } = render(<DashboardHeader onFilterClick={onFilterClick}>Welcome</DashboardHeader>);
 
     const element = getByText('Welcome');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  test('render emoji', () => {
+    const onFilterClick = jest.fn();
+    const { getByAltText } = render(<DashboardHeader onFilterClick={onFilterClick}>🤔</DashboardHeader>);
+
+    const element = getByAltText('🤔');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  test('render date time correctly', () => {
+    const onFilterClick = jest.fn();
+    const { getByAltText } = render(<DashboardHeader onFilterClick={onFilterClick}>🤔</DashboardHeader>);
+
+    const element = getByAltText(DateTime.local().toLocaleString(DateTime.DATE_HUGE));
 
     expect(element).toBeInTheDocument();
   });
