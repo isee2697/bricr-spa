a6084fcd433d87a6112832a26d1649c559fefa1f
isee2697 <isee2697@outlook.com>
2021-03-16 13:24:38 -0400

    UL-3121 Action tabs test

---
+++ src/ui/molecules/actionTabs/ActionTabs.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import { render } from 'tests';
+
+import { ActionTabs } from './ActionTabs';
+import { ActionTab } from './ActionTabs.types';
+
+describe('ActionTabs', () => {
+  test('render correctly', () => {
+    const tabs: ActionTab[] = [
+      {
+        value: 'active',
+        label: 'common.active',
+      },
+      {
+        value: 'inactive',
+        label: 'common.inactive',
+      },
+    ];
+
+    const { getByText } = render(<ActionTabs tabs={tabs} status="active" onStatusChange={() => {}} />);
+
+    expect(getByText('common.active')).toBeInTheDocument();
+    expect(getByText('common.inactive')).toBeInTheDocument();
+  });
+
+  test('render count numbers', () => {
+    const tabs: ActionTab[] = [
+      {
+        value: 'active',
+        label: 'common.active',
+        amount: 12,
+      },
+      {
+        value: 'inactive',
+        label: 'common.inactive',
+        amount: 28,
+      },
+    ];
+
+    const { getByText } = render(<ActionTabs tabs={tabs} status="active" onStatusChange={() => {}} />);
+
+    expect(getByText('common.active (12)')).toBeInTheDocument();
+    expect(getByText('common.inactive (28)')).toBeInTheDocument();
+  });
+
+  test('render count numbers in badge', () => {
+    const tabs: ActionTab[] = [
+      {
+        value: 'active',
+        label: 'common.active',
+        amount: 12,
+        hasBadge: true,
+      },
+      {
+        value: 'inactive',
+        label: 'common.inactive',
+        amount: 28,
+        hasBadge: true,
+      },
+    ];
+
+    const { getByText } = render(<ActionTabs tabs={tabs} status="active" onStatusChange={() => {}} />);
+
+    expect(getByText('12')).toBeInTheDocument();
+    expect(getByText('28')).toBeInTheDocument();
+  });
+});
