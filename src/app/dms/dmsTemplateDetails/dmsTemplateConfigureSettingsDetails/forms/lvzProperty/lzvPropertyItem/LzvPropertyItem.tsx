7a7776472e126b25840fbaafead760f32deafe01
iamwkev <iamwkev@gmail.com>
2021-03-16 07:53:02 -0400

    Fix cardwithlist component and use it in other places

--- src/app/dms/dmsTemplateDetails/dmsTemplateConfigureSettingsDetails/forms/lvzProperty/lzvPropertyItem/LzvPropertyItem.tsx
+++ src/app/dms/dmsTemplateDetails/dmsTemplateConfigureSettingsDetails/forms/lvzProperty/lzvPropertyItem/LzvPropertyItem.tsx
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 
-import { Box, Card, CardContent, CardHeader, FormControlLabel, Grid, IconButton, Switch, Typography } from 'ui/atoms';
-import { AddIcon, ArrowDownIcon, ArrowUpIcon, ClockIcon, MenuIcon } from 'ui/atoms/icons';
+import { Grid, Menu } from 'ui/atoms';
+import { ClockIcon, DeleteIcon } from 'ui/atoms/icons';
 import { useLocale, useModalDispatch, useModalState } from 'hooks';
 import { AutosaveForm } from 'ui/organisms';
 import { GenericField } from 'form/fields';
 import { AddLvzPropertyGroupItemModal } from '../addLvzPropertyGroupItemModal/AddLvzPropertyGroupItemModal';
 import { AddLvzPropertyGroupItemBody } from '../addLvzPropertyGroupItemModal/AddLvzPropertyGroupItemModal.types';
-import { InfoSection, ListOptionsMenu } from 'ui/molecules';
 import { ListOptionsMenuItem } from 'ui/molecules/listOptionsMenu/menuItem/ListOptionsMenuItem';
+import { CardWithList } from 'ui/templates';
 
 import { LvzPropertyGroupItem, LzvPropertyItemProps } from './LzvPropertyItem.types';
-import { useStyles } from './LzvPropertyItem.styles';
 
 export const LzvPropertyItem = ({ group }: LzvPropertyItemProps) => {
   const { formatMessage } = useLocale();
-  const [isEditing, setIsEditing] = useState(false);
-  const [isExpanded, setIsExpanded] = useState(true);
   const [lvzGroupItems, setLvzGroupItems] = useState<LvzPropertyGroupItem[]>([]);
-  const classes = useStyles();
   const { open, close } = useModalDispatch();
   const { isOpen: isModalOpen } = useModalState('add-lvz-property-group-item');
 
@@ -26,6 +22,17 @@ export const LzvPropertyItem = ({ group }: LzvPropertyItemProps) => {
 
   const initialValues = {
     name,
+  };
+
+  const [menuEl, setMenuEl] = useState<HTMLElement | null>(null);
+
+  const onMenuClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.stopPropagation();
+    setMenuEl(menuEl ? null : event.currentTarget);
+  };
+
+  const onMenuClose = () => {
+    setMenuEl(null);
   };
 
   const handleSave = async () => {
@@ -46,98 +53,65 @@ export const LzvPropertyItem = ({ group }: LzvPropertyItemProps) => {
     return undefined;
   };
 
+  const handleSaveSubItem = async (value: LvzPropertyGroupItem) => {
+    // TODO: Save group sub item
+
+    return undefined;
+  };
+
   return (
     <>
-      <Card className={classes.root}>
-        <CardHeader
-          title={name}
-          action={
-            <Box display="flex" alignItems="center">
-              <FormControlLabel
-                control={<Switch checked={isEditing} onChange={() => setIsEditing(!isEditing)} color="primary" />}
-                label={formatMessage({ id: 'form_section.edit_mode' })}
-                labelPlacement="start"
+      <CardWithList
+        emoji="🤔"
+        emptyStateTextFirst={formatMessage({ id: 'dms.templates.lvz_property.item.empty.title' })}
+        emptyStateTextSecond={formatMessage({ id: 'dms.templates.lvz_property.item.empty.description' })}
+        title={name}
+        items={lvzGroupItems}
+        onSave={(value: LvzPropertyGroupItem) => handleSaveSubItem(value)}
+        onAdd={() => open('add-lvz-property-group-item')}
+        onOptionsClick={onMenuClick}
+        renderGenericFields={isEditing => (
+          <AutosaveForm onSave={handleSave} initialValues={initialValues}>
+            <Grid item xs={12}>
+              <GenericField
+                fullWidth
+                name="name"
+                label={formatMessage({ id: 'dms.templates.lvz_property.item.name_lvz_group' })}
+                placeholder={formatMessage({ id: 'dms.templates.lvz_property.item.name_lvz_group.placeholder' })}
+                disabled={!isEditing}
               />
-              <Box ml={1.5} />
-              <IconButton
-                aria-label="add"
-                color="primary"
-                size="small"
-                onClick={() => open('add-lvz-property-group-item')}
-              >
-                <AddIcon color="inherit" />
-              </IconButton>
-              <Box ml={1.5} />
-              <ListOptionsMenu id={`lvz-property-item-${id}`} onDeleteClick={() => {}} hideEditButton>
-                <ListOptionsMenuItem
-                  title={formatMessage({
-                    id: 'dms.templates.clone',
-                  })}
-                  icon={<ClockIcon />}
-                />
-                <ListOptionsMenuItem
-                  title={formatMessage({
-                    id: 'dms.templates.inactive',
-                  })}
-                  icon={<ClockIcon />}
-                />
-              </ListOptionsMenu>
-              <Box ml={1.5} />
-              <IconButton size="small" variant="roundedContained" onClick={() => setIsExpanded(!isExpanded)}>
-                {isExpanded ? <ArrowUpIcon /> : <ArrowDownIcon />}
-              </IconButton>
-            </Box>
-          }
-        />
-        {isExpanded ? (
-          <CardContent>
-            <AutosaveForm onSave={handleSave} initialValues={initialValues}>
-              <Grid item xs={12}>
-                <GenericField
-                  fullWidth
-                  name="name"
-                  label={formatMessage({ id: 'dms.templates.lvz_property.item.name_lvz_group' })}
-                  placeholder={formatMessage({ id: 'dms.templates.lvz_property.item.name_lvz_group.placeholder' })}
-                  disabled={!isEditing}
-                />
-              </Grid>
-            </AutosaveForm>
-            {lvzGroupItems.length === 0 && (
-              <InfoSection emoji="🤔">
-                <Typography variant="h3">
-                  {formatMessage({ id: 'dms.templates.lvz_property.item.empty.title' })}
-                </Typography>
-                <Typography variant="h3">
-                  {formatMessage({ id: 'dms.templates.lvz_property.item.empty.description' })}
-                </Typography>
-              </InfoSection>
-            )}
-            {lvzGroupItems.map((item, index) => (
-              <Box
-                mt={3}
-                width="100%"
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                className={classes.itemRow}
-              >
-                <Box display="flex">
-                  <Typography variant="h4" color="textSecondary" className={classes.counter}>
-                    {index + 1}
-                  </Typography>
-                  <Box ml={0.5} />
-                  <Typography variant="h3">{item.name}</Typography>
-                </Box>
-                <IconButton size="small" variant="rounded">
-                  <MenuIcon />
-                </IconButton>
-              </Box>
-            ))}
-          </CardContent>
-        ) : (
-          <Box mt={2} />
+            </Grid>
+          </AutosaveForm>
         )}
-      </Card>
+        renderItem={(item, isEditing) => <></>}
+      />
+      <Menu
+        id={`lvz-property-item-${id}`}
+        open={Boolean(menuEl)}
+        onClose={onMenuClose}
+        anchorEl={menuEl}
+        placement="bottom-end"
+      >
+        <ListOptionsMenuItem
+          title={formatMessage({
+            id: 'dms.templates.clone',
+          })}
+          icon={<ClockIcon />}
+        />
+        <ListOptionsMenuItem
+          title={formatMessage({
+            id: 'dms.templates.inactive',
+          })}
+          icon={<ClockIcon />}
+        />
+        <ListOptionsMenuItem
+          title={formatMessage({
+            id: 'common.delete',
+          })}
+          icon={<DeleteIcon color="secondary" />}
+          color="secondary"
+        />
+      </Menu>
       <AddLvzPropertyGroupItemModal
         isOpened={isModalOpen}
         onClose={() => close('add-lvz-property-group-item')}
--- src/app/dms/dmsTemplateDetails/dmsTemplateConfigureSettingsDetails/forms/questionnaireProperty/questionnaireItem/item/Item.tsx
+++ src/app/dms/dmsTemplateDetails/dmsTemplateConfigureSettingsDetails/forms/questionnaireProperty/questionnaireItem/item/Item.tsx
@@ -1,85 +1,64 @@
 import React from 'react';
 
-import { AutosaveForm, FormSubSection } from 'ui/organisms';
 import { FormSubSectionHeader } from 'ui/molecules';
 import { CheckboxField, GenericField, RadioGroupField } from 'form/fields';
 import { questionTypes } from '../../addQuestionnaireGroupItemModal/dictionaries';
 import { Grid, Box } from 'ui/atoms';
 import { QuestionnaireItemAnswers } from '../questionnaireItemAnswers/QuestionnaireItemAnswers';
 import { useLocale } from 'hooks';
-import { QuestionnaireGroupItem } from '../QuestionnaireItem.types';
 
 import { QuestionnaireItemSubItemProps } from './Item.types';
 
 export const QuestionnaireItemSubItem = ({
-  index,
-  item: {
-    questionType,
-    question,
-    isAnswerRequired,
-    isShowOnSummaryPage,
-    isSupplyExtraCommentField,
-    multipleChoiceAnswers = [],
-    questionsOnlyForObjectType,
-  },
-  onSave,
+  item: { multipleChoiceAnswers = [] },
+  isEditing,
 }: QuestionnaireItemSubItemProps) => {
   const { formatMessage } = useLocale();
-  const handleSave = async (body: QuestionnaireGroupItem) => {
-    onSave(body);
-
-    return undefined;
-  };
-
-  const initialValues = {
-    questionType,
-    question,
-    isAnswerRequired,
-    isShowOnSummaryPage,
-    isSupplyExtraCommentField,
-    multipleChoiceAnswers,
-    questionsOnlyForObjectType,
-  };
 
   return (
-    <AutosaveForm onSave={handleSave} initialValues={{ ...initialValues }}>
-      <FormSubSection title={question} onOptionsClick={() => {}} counter={index + 1}>
-        <FormSubSectionHeader
-          title={formatMessage({ id: 'dms.templates.questionnaire.type_of_question' })}
-          subtitle={formatMessage({ id: 'common.choose_one_option_below' })}
-          noBorder
-        />
-        <RadioGroupField xs={2} name="questionType" options={questionTypes} />
-        <Grid container spacing={1}>
-          <Grid item xs={4}>
-            <CheckboxField
-              label={formatMessage({ id: 'dms.templates.questionnaire.isAnswerRequired' })}
-              name="isAnswerRequired"
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <CheckboxField
-              label={formatMessage({ id: 'dms.templates.questionnaire.isSupplyExtraCommentField' })}
-              name="isSupplyExtraCommentField"
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <CheckboxField
-              label={formatMessage({ id: 'dms.templates.questionnaire.isShowOnSummaryPage' })}
-              name="isShowOnSummaryPage"
-            />
-          </Grid>
+    <>
+      <FormSubSectionHeader
+        title={formatMessage({ id: 'dms.templates.questionnaire.type_of_question' })}
+        subtitle={formatMessage({ id: 'common.choose_one_option_below' })}
+        noBorder
+      />
+      <RadioGroupField xs={2} name="questionType" options={questionTypes} disabled={!isEditing} />
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+          <CheckboxField
+            label={formatMessage({ id: 'dms.templates.questionnaire.isAnswerRequired' })}
+            name="isAnswerRequired"
+            disabled={!isEditing}
+          />
         </Grid>
-        <GenericField name="question" label={formatMessage({ id: 'dms.templates.questionnaire.question' })} />
-        <QuestionnaireItemAnswers answers={multipleChoiceAnswers} />
-        <Box mt={2} />
-        <FormSubSectionHeader
-          title={formatMessage({ id: 'dms.templates.questionnaire.question_only_for_type_of_object' })}
-          subtitle={formatMessage({ id: 'common.choose_one_option_below' })}
-          noBorder
-        />
-        <RadioGroupField xs={2} name="questionsOnlyForObjectType" options={questionTypes} />
-      </FormSubSection>
-    </AutosaveForm>
+        <Grid item xs={4}>
+          <CheckboxField
+            label={formatMessage({ id: 'dms.templates.questionnaire.isSupplyExtraCommentField' })}
+            name="isSupplyExtraCommentField"
+            disabled={!isEditing}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <CheckboxField
+            label={formatMessage({ id: 'dms.templates.questionnaire.isShowOnSummaryPage' })}
+            name="isShowOnSummaryPage"
+            disabled={!isEditing}
+          />
+        </Grid>
+      </Grid>
+      <GenericField
+        name="question"
+        label={formatMessage({ id: 'dms.templates.questionnaire.question' })}
+        disabled={!isEditing}
+      />
+      <QuestionnaireItemAnswers answers={multipleChoiceAnswers} />
+      <Box mt={2} />
+      <FormSubSectionHeader
+        title={formatMessage({ id: 'dms.templates.questionnaire.question_only_for_type_of_object' })}
+        subtitle={formatMessage({ id: 'common.choose_one_option_below' })}
+        noBorder
+      />
+      <RadioGroupField xs={2} name="questionsOnlyForObjectType" options={questionTypes} disabled={!isEditing} />
+    </>
   );
 };
