04dd1b07900bd1f29206021b3d5b98c6c4f99227
pgeoff <pgeoff057@gmail.com>
2021-03-16 21:58:58 -0400

    UL-3127 Color testing

--- src/ui/molecules/confirmModal/ConfirmModal.test.tsx
+++ src/ui/molecules/confirmModal/ConfirmModal.test.tsx
@@ -1,6 +1,8 @@
+import { SimplePaletteColorOptions } from '@material-ui/core';
 import React from 'react';
 
 import { render, act, wait, fireEvent } from 'tests';
+import { palette } from 'theme/palette';
 
 import { ConfirmModal } from './ConfirmModal';
 import { ConfirmButtonType } from './ConfirmModal.types';
@@ -30,6 +32,9 @@ describe('ConfirmModal', () => {
     expect(getByText('Yes')).toBeInTheDocument();
     expect(getByText('First line message')).toBeInTheDocument();
     expect(getByText('Second line message')).toBeInTheDocument();
+    expect(getByText('Yes').parentElement?.parentElement).toHaveStyle(
+      `color: ${(palette.secondary as SimplePaletteColorOptions).main}`,
+    );
 
     act(() => {
       fireEvent.click(getByText('No'));
@@ -46,5 +51,29 @@ describe('ConfirmModal', () => {
     wait(() => {
       expect(onConfirm).toBeCalled();
     });
+  });
+
+  test('render info confirm button', () => {
+    const onCancel = jest.fn();
+    const onConfirm = jest.fn();
+
+    const { getByText } = render(
+      <ConfirmModal
+        emoji="😬"
+        isOpened={true}
+        title={'Test Confirm Modal'}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
+        messageLineFirst={'First line message'}
+        messageLineSecond={'Second line message'}
+        cancelText={'No'}
+        confirmText={'Yes'}
+        confirmButtonType={ConfirmButtonType.INFO}
+      />,
+    );
+
+    expect(getByText('Yes').parentElement?.parentElement).toHaveStyle(
+      `background-color: ${(palette.primary as SimplePaletteColorOptions).main}`,
+    );
   });
 });
