95b5b53f270f115944fded110fd72ae7e16aacd5
isee2697 <isee2697@outlook.com>
2021-03-22 12:33:16 -0400

    UL-3095 Exit button route change test

--- src/ui/atoms/exitButton/ExitButton.test.tsx
+++ src/ui/atoms/exitButton/ExitButton.test.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Route, MemoryRouter } from 'react-router-dom';
 
-import { render } from 'tests';
+import { render, act, fireEvent, wait } from 'tests';
 
 import { ExitButton } from './ExitButton';
 
 describe('ExitButton', () => {
   it('render correctly', () => {
-    const { getByLabelText } = render(<ExitButton />);
+    const { getByLabelText, getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <Route exact path="/">
+          <ExitButton to="/other" />
+        </Route>
+        <Route exact path="/other">
+          <div>Other content</div>
+        </Route>
+      </MemoryRouter>,
+    );
 
     expect(getByLabelText('back')).toBeTruthy();
+
+    act(() => {
+      fireEvent.click(getByLabelText('back'));
+    });
+
+    wait(() => {
+      expect(getByText('Other content')).toBeInTheDocument();
+    });
   });
 });
